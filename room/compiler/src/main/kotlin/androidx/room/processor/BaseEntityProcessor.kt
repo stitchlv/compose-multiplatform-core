aosp-mirror/platform_frameworks_support

Room FTS Support - Processor
Created FtsEntityProcessor to process classes annotated with @FtsEntity.
EntityProcessor & FtsEntityProcessor now implement a common interface
BaseEntityProcessor that will either create the Entity or FtsEntity
value object based on the element annotation. FtsEntity extends Entity
which keep intact most of the Entity class reference around the compiler
code .

Created migration bundle class for FtsEntity to later handle migration.

Bug: 62356416
Test: ./gradlew room:room-compiler:test \
                room:integration-tests:testapp:cC

Change-Id: I867635165b7e1dcb37806273c1c097fa4f450408

room/compiler/src/main/kotlin/androidx/room/processor/BaseEntityProcessor.kt
@@ -1,0 +1,54 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.room.processor
+
+import androidx.room.FtsEntity
+import androidx.room.ext.hasAnnotation
+import androidx.room.vo.Entity
+import com.google.auto.common.AnnotationMirrors
+import javax.lang.model.element.AnnotationMirror
+import javax.lang.model.element.Name
+import javax.lang.model.element.TypeElement
+
+interface BaseEntityProcessor {
+    fun process(): Entity
+
+    companion object {
+
+        fun createFor(
+            context: Context,
+            element: TypeElement,
+            referenceStack: LinkedHashSet<Name> = LinkedHashSet()
+        ): BaseEntityProcessor {
+            return if (element.hasAnnotation(FtsEntity::class)) {
+                FtsEntityProcessor(context, element, referenceStack)
+            } else {
+                EntityProcessor(context, element, referenceStack)
+            }
+        }
+
+        fun extractTableName(element: TypeElement, annotation: AnnotationMirror): String {
+            val annotationValue = AnnotationMirrors
+                    .getAnnotationValue(annotation, "tableName").value.toString()
+            return if (annotationValue == "") {
+                element.simpleName.toString()
+            } else {
+                annotationValue
+            }
+        }
+    }
+}

room/integration-tests/autovaluetestapp/src/androidTest/java/androidx/room/integration/autovaluetestapp/vo/Mail.java
@@ -1,0 +1,32 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.room.integration.autovaluetestapp.vo;
+
+import androidx.room.FtsEntity;
+
+import com.google.auto.value.AutoValue;
+
+@AutoValue
+@FtsEntity
+public abstract class Mail {
+    abstract String getSubject();
+    abstract String getBody();
+
+    public static Mail create(String subject, String body) {
+        return new AutoValue_Mail(subject, body);
+    }
+}

room/integration-tests/testapp/src/androidTest/java/androidx/room/integration/testapp/vo/Mail.java
@@ -1,0 +1,65 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.room.integration.testapp.vo;
+
+import androidx.room.ColumnInfo;
+import androidx.room.FtsEntity;
+import androidx.room.PrimaryKey;
+
+@FtsEntity()
+public class Mail {
+
+    @PrimaryKey
+    @ColumnInfo(name = "rowid")
+    public long rowId;
+
+    public String subject;
+    public String body;
+    public long datetime;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Mail mail = (Mail) o;
+
+        if (rowId != mail.rowId) return false;
+        if (datetime != mail.datetime) return false;
+        if (subject != null ? !subject.equals(mail.subject) : mail.subject != null) return false;
+        return body != null ? body.equals(mail.body) : mail.body == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (rowId ^ (rowId >>> 32));
+        result = 31 * result + (subject != null ? subject.hashCode() : 0);
+        result = 31 * result + (body != null ? body.hashCode() : 0);
+        result = 31 * result + (int) (datetime ^ (datetime >>> 32));
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Mail{"
+                + "rowId=" + rowId
+                + ", subject='" + subject + '\''
+                + ", body='" + body + '\''
+                + ", datetime=" + datetime
+                + '}';
+    }
+}

room/migration/src/main/java/androidx/room/migration/bundle/FtsEntityBundle.java
@@ -1,0 +1,86 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.room.migration.bundle;
+
+import static androidx.room.migration.bundle.SchemaEqualityUtil.checkSchemaEquality;
+
+import androidx.annotation.RestrictTo;
+import androidx.room.FtsEntity;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Data class that holds the schema information about an
+ * {@link FtsEntity FtsEntity}.
+ *
+ * @hide
+ */
+@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+public class FtsEntityBundle extends EntityBundle {
+
+    private static final String [] SHADOW_TABLE_NAME_SUFFIXES = {
+            "_content", "_segdir", "_segments", "_stat", "_docsize"
+    };
+
+    @SerializedName("ftsVersion")
+    private final String mFtsVersion;
+
+    @SerializedName("ftsOptions")
+    private final FtsOptionsBundle mFtsOptions;
+
+    public FtsEntityBundle(
+            String tableName,
+            String createSql,
+            List<FieldBundle> fields,
+            PrimaryKeyBundle primaryKey,
+            String ftsVersion,
+            FtsOptionsBundle ftsOptions) {
+        super(tableName, createSql, fields, primaryKey, Collections.<IndexBundle>emptyList(),
+                Collections.<ForeignKeyBundle>emptyList());
+        mFtsVersion = ftsVersion;
+        mFtsOptions = ftsOptions;
+    }
+
+    @Override
+    public boolean isSchemaEqual(EntityBundle other) {
+        boolean isSuperSchemaEqual = super.isSchemaEqual(other);
+        if (other instanceof FtsEntityBundle) {
+            FtsEntityBundle otherFtsBundle = (FtsEntityBundle) other;
+            return isSuperSchemaEqual
+                    && mFtsVersion.equals(otherFtsBundle.mFtsVersion)
+                    && checkSchemaEquality(mFtsOptions, otherFtsBundle.mFtsOptions);
+        } else {
+            return isSuperSchemaEqual;
+        }
+    }
+
+    /**
+     * Gets the list of shadow table names corresponding to the FTS virtual table.
+     * @return the list of names.
+     */
+    public List<String> getShadowTableNames() {
+        List<String> names = new ArrayList<>(SHADOW_TABLE_NAME_SUFFIXES.length);
+        for (String suffix : SHADOW_TABLE_NAME_SUFFIXES) {
+            names.add(getTableName() + suffix);
+        }
+        return names;
+    }
+}

stitchlv/compose-multiplatform-core

room/compiler/src/main/kotlin/androidx/room/processor/BaseEntityProcessor.kt
@@ -1,0 +1,54 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.room.processor
+
+import androidx.room.Fts3Entity
+import androidx.room.Fts4Entity
+import androidx.room.ext.hasAnyOf
+import androidx.room.vo.Entity
+import com.google.auto.common.AnnotationMirrors
+import javax.lang.model.element.AnnotationMirror
+import javax.lang.model.element.Name
+import javax.lang.model.element.TypeElement
+
+interface BaseEntityProcessor {
+    fun process(): Entity
+
+    companion object {
+        fun extractTableName(element: TypeElement, annotation: AnnotationMirror): String {
+            val annotationValue = AnnotationMirrors
+                    .getAnnotationValue(annotation, "tableName").value.toString()
+            return if (annotationValue == "") {
+                element.simpleName.toString()
+            } else {
+                annotationValue
+            }
+        }
+    }
+}
+
+fun EntityProcessor(
+    context: Context,
+    element: TypeElement,
+    referenceStack: LinkedHashSet<Name> = LinkedHashSet()
+): BaseEntityProcessor {
+    return if (element.hasAnyOf(Fts3Entity::class, Fts4Entity::class)) {
+        FtsTableEntityProcessor(context, element, referenceStack)
+    } else {
+        TableEntityProcessor(context, element, referenceStack)
+    }
+}

room/integration-tests/autovaluetestapp/src/androidTest/java/androidx/room/integration/autovaluetestapp/vo/Mail.java
@@ -1,0 +1,32 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.room.integration.autovaluetestapp.vo;
+
+import androidx.room.Fts4Entity;
+
+import com.google.auto.value.AutoValue;
+
+@AutoValue
+@Fts4Entity
+public abstract class Mail {
+    abstract String getSubject();
+    abstract String getBody();
+
+    public static Mail create(String subject, String body) {
+        return new AutoValue_Mail(subject, body);
+    }
+}

room/integration-tests/testapp/src/androidTest/java/androidx/room/integration/testapp/vo/Mail.java
@@ -1,0 +1,65 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.room.integration.testapp.vo;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Fts4Entity;
+import androidx.room.PrimaryKey;
+
+@Fts4Entity
+public class Mail {
+
+    @PrimaryKey
+    @ColumnInfo(name = "rowid")
+    public long rowId;
+
+    public String subject;
+    public String body;
+    public long datetime;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Mail mail = (Mail) o;
+
+        if (rowId != mail.rowId) return false;
+        if (datetime != mail.datetime) return false;
+        if (subject != null ? !subject.equals(mail.subject) : mail.subject != null) return false;
+        return body != null ? body.equals(mail.body) : mail.body == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (rowId ^ (rowId >>> 32));
+        result = 31 * result + (subject != null ? subject.hashCode() : 0);
+        result = 31 * result + (body != null ? body.hashCode() : 0);
+        result = 31 * result + (int) (datetime ^ (datetime >>> 32));
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Mail{"
+                + "rowId=" + rowId
+                + ", subject='" + subject + '\''
+                + ", body='" + body + '\''
+                + ", datetime=" + datetime
+                + '}';
+    }
+}

room/migration/src/main/java/androidx/room/migration/bundle/FtsEntityBundle.java
@@ -1,0 +1,87 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.room.migration.bundle;
+
+import static androidx.room.migration.bundle.SchemaEqualityUtil.checkSchemaEquality;
+
+import androidx.annotation.RestrictTo;
+import androidx.room.Fts3Entity;
+import androidx.room.Fts4Entity;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Data class that holds the schema information about an
+ * {@link Fts3Entity Fts3Entity} or {@link Fts4Entity Fts4Entity}.
+ *
+ * @hide
+ */
+@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+public class FtsEntityBundle extends EntityBundle {
+
+    private static final String [] SHADOW_TABLE_NAME_SUFFIXES = {
+            "_content", "_segdir", "_segments", "_stat", "_docsize"
+    };
+
+    @SerializedName("ftsVersion")
+    private final String mFtsVersion;
+
+    @SerializedName("ftsOptions")
+    private final FtsOptionsBundle mFtsOptions;
+
+    public FtsEntityBundle(
+            String tableName,
+            String createSql,
+            List<FieldBundle> fields,
+            PrimaryKeyBundle primaryKey,
+            String ftsVersion,
+            FtsOptionsBundle ftsOptions) {
+        super(tableName, createSql, fields, primaryKey, Collections.<IndexBundle>emptyList(),
+                Collections.<ForeignKeyBundle>emptyList());
+        mFtsVersion = ftsVersion;
+        mFtsOptions = ftsOptions;
+    }
+
+    @Override
+    public boolean isSchemaEqual(EntityBundle other) {
+        boolean isSuperSchemaEqual = super.isSchemaEqual(other);
+        if (other instanceof FtsEntityBundle) {
+            FtsEntityBundle otherFtsBundle = (FtsEntityBundle) other;
+            return isSuperSchemaEqual
+                    && mFtsVersion.equals(otherFtsBundle.mFtsVersion)
+                    && checkSchemaEquality(mFtsOptions, otherFtsBundle.mFtsOptions);
+        } else {
+            return isSuperSchemaEqual;
+        }
+    }
+
+    /**
+     * Gets the list of shadow table names corresponding to the FTS virtual table.
+     * @return the list of names.
+     */
+    public List<String> getShadowTableNames() {
+        List<String> names = new ArrayList<>(SHADOW_TABLE_NAME_SUFFIXES.length);
+        for (String suffix : SHADOW_TABLE_NAME_SUFFIXES) {
+            names.add(getTableName() + suffix);
+        }
+        return names;
+    }
+}