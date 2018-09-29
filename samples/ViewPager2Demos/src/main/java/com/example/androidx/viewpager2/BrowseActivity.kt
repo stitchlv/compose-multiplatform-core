aosp-mirror/platform_frameworks_support

Converting the ViewPager2 sample from Java to Kotlin and simplifying the BrowseActivity
Bug: 116082062
Test: N/A
Change-Id: I5b9b6f45425533ce50a7d0a78fa96408eab7e8e1

samples/ViewPager2Demos/src/main/java/com/example/androidx/viewpager2/BrowseActivity.kt
@@ -1,0 +1,60 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.androidx.viewpager2
+
+import android.app.ListActivity
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.ListView
+import android.widget.SimpleAdapter
+
+/**
+ * This activity lists all the activities in this application.
+ */
+class BrowseActivity : ListActivity() {
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        listAdapter = SimpleAdapter(this, getData(),
+                android.R.layout.simple_list_item_1, arrayOf("title"),
+                intArrayOf(android.R.id.text1))
+    }
+
+    private fun getData(): List<Map<String, Any>> {
+        val myData = mutableListOf<Map<String, Any>>()
+
+        myData.add(hashMapOf("title" to CardActivity::class.java.simpleName,
+                "intent" to activityToIntent(CardActivity::class.java.name)))
+        myData.add(hashMapOf("title" to CardFragmentActivity::class.java.simpleName,
+                "intent" to activityToIntent(CardFragmentActivity::class.java.name)))
+
+        return myData
+    }
+
+    private fun activityToIntent(activity: String): Intent =
+            Intent(Intent.ACTION_VIEW).setClassName(this.packageName, activity)
+
+    override fun onListItemClick(l: ListView, v: View, position: Int, id: Long) {
+        val map = l.getItemAtPosition(position) as Map<String, Any>
+
+        val intent = Intent(map["intent"] as Intent)
+        intent.addCategory(Intent.CATEGORY_SAMPLE_CODE)
+        startActivity(intent)
+    }
+}

samples/ViewPager2Demos/src/main/java/com/example/androidx/viewpager2/CardFragmentActivity.kt
@@ -1,0 +1,75 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.androidx.viewpager2
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentProvider
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+import com.example.androidx.viewpager2.cards.Card
+import com.example.androidx.viewpager2.cards.CardView
+
+/**
+ * Shows how to use a [androidx.viewpager2.widget.ViewPager2] with Fragments, via a
+ * [androidx.viewpager2.adapter.FragmentStateAdapter]
+ *
+ * @see CardActivity for an example of using {@link androidx.viewpager2.widget.ViewPager2} with
+ * Views.
+ */
+class CardFragmentActivity : BaseCardActivity() {
+
+    override fun onCreate(bundle: Bundle?) {
+        super.onCreate(bundle)
+
+        viewPager.adapter = FragmentStateAdapter(supportFragmentManager,
+                object : FragmentProvider {
+                    override fun getItem(position: Int): Fragment {
+                        return CardFragment.create(BaseCardActivity.cards[position])
+                    }
+
+                    override fun getCount(): Int {
+                        return BaseCardActivity.cards.size
+                    }
+                })
+    }
+
+    class CardFragment : Fragment() {
+        override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
+            val cardView = CardView(layoutInflater, container!!)
+            cardView.bind(Card.fromBundle(arguments!!))
+            return cardView.view
+        }
+
+        companion object {
+
+            /** Creates a Fragment for a given [Card]  */
+            fun create(card: Card): CardFragment {
+                val fragment = CardFragment()
+                fragment.arguments = card.toBundle()
+                return fragment
+            }
+        }
+    }
+}

stitchlv/compose-multiplatform-core

Test: manual

samples/ViewPager2Demos/src/main/java/com/example/androidx/viewpager2/BrowseActivity.kt
@@ -1,0 +1,60 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.androidx.viewpager2
+
+import android.app.ListActivity
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.ListView
+import android.widget.SimpleAdapter
+
+/**
+ * This activity lists all the activities in this application.
+ */
+class BrowseActivity : ListActivity() {
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        listAdapter = SimpleAdapter(this, getData(),
+                android.R.layout.simple_list_item_1, arrayOf("title"),
+                intArrayOf(android.R.id.text1))
+    }
+
+    private fun getData(): List<Map<String, Any>> {
+        val myData = mutableListOf<Map<String, Any>>()
+
+        myData.add(hashMapOf("title" to CardViewActivity::class.java.simpleName,
+                "intent" to activityToIntent(CardViewActivity::class.java.name)))
+        myData.add(hashMapOf("title" to CardFragmentActivity::class.java.simpleName,
+                "intent" to activityToIntent(CardFragmentActivity::class.java.name)))
+
+        return myData
+    }
+
+    private fun activityToIntent(activity: String): Intent =
+            Intent(Intent.ACTION_VIEW).setClassName(this.packageName, activity)
+
+    override fun onListItemClick(listView: ListView, view: View, position: Int, id: Long) {
+        val map = listView.getItemAtPosition(position) as Map<String, Any>
+
+        val intent = Intent(map["intent"] as Intent)
+        intent.addCategory(Intent.CATEGORY_SAMPLE_CODE)
+        startActivity(intent)
+    }
+}

samples/ViewPager2Demos/src/main/java/com/example/androidx/viewpager2/CardFragmentActivity.kt
@@ -1,0 +1,73 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.androidx.viewpager2
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+import com.example.androidx.viewpager2.cards.Card
+import com.example.androidx.viewpager2.cards.CardView
+
+/**
+ * Shows how to use a [androidx.viewpager2.widget.ViewPager2] with Fragments, via a
+ * [androidx.viewpager2.adapter.FragmentStateAdapter]
+ *
+ * @see CardViewActivity for an example of using {@link androidx.viewpager2.widget.ViewPager2} with
+ * Views.
+ */
+class CardFragmentActivity : BaseCardActivity() {
+
+    override fun onCreate(bundle: Bundle?) {
+        super.onCreate(bundle)
+
+        viewPager.adapter = object : FragmentStateAdapter(supportFragmentManager) {
+            override fun getItem(position: Int): Fragment {
+                return CardFragment.create(BaseCardActivity.cards[position])
+            }
+
+            override fun getItemCount(): Int {
+                return BaseCardActivity.cards.size
+            }
+        }
+    }
+
+    class CardFragment : Fragment() {
+        override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
+            val cardView = CardView(layoutInflater, container!!)
+            cardView.bind(Card.fromBundle(arguments!!))
+            return cardView.view
+        }
+
+        companion object {
+
+            /** Creates a Fragment for a given [Card]  */
+            fun create(card: Card): CardFragment {
+                val fragment = CardFragment()
+                fragment.arguments = card.toBundle()
+                return fragment
+            }
+        }
+    }
+}