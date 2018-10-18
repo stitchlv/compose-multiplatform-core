aosp-mirror/platform_frameworks_support

Allow use of view in @Relation
Bug: 117680932
Test: PojoProcessorTest, DatabaseViewTest
Change-Id: I99a5843eacae44f56845a5e709952d09dad4c3ee

room/compiler/src/main/kotlin/androidx/room/processor/EntityProcessor.kt
@@ -28,8 +28,8 @@ import javax.lang.model.element.Name
 import javax.lang.model.element.TypeElement
 import javax.lang.model.type.TypeMirror
 
-interface EntityProcessor {
-    fun process(): androidx.room.vo.Entity
+interface EntityProcessor : DatabaseItemProcessor {
+    override fun process(): androidx.room.vo.Entity
 
     companion object {
         fun extractTableName(element: TypeElement, annotation: Entity): String {

room/compiler/src/main/kotlin/androidx/room/processor/PojoProcessor.kt
@@ -37,8 +37,8 @@ import androidx.room.processor.autovalue.AutoValuePojoProcessorDelegate
 import androidx.room.processor.cache.Cache
 import androidx.room.vo.CallType
 import androidx.room.vo.Constructor
+import androidx.room.vo.DatabaseItem
 import androidx.room.vo.EmbeddedField
-import androidx.room.vo.Entity
 import androidx.room.vo.Field
 import androidx.room.vo.FieldGetter
 import androidx.room.vo.FieldSetter
@@ -499,7 +499,7 @@ class PojoProcessor private constructor(
             return null
         }
 
-        val entity = EntityProcessor(context, entityElement, referenceStack).process()
+        val entity = databaseItemProcessor(context, entityElement, referenceStack).process()
 
         // now find the field in the entity.
         val entityField = entity.fields.firstOrNull {
@@ -543,7 +543,7 @@ class PojoProcessor private constructor(
 
     private fun validateRelationshipProjection(
         projectionInput: Array<String>,
-        entity: Entity,
+        entity: DatabaseItem,
         relationElement: VariableElement
     ) {
         val missingColumns = projectionInput.filterNot { columnName ->
@@ -568,7 +568,7 @@ class PojoProcessor private constructor(
     private fun createRelationshipProjection(
         inferEntity: Boolean,
         typeArg: TypeMirror,
-        entity: Entity,
+        entity: DatabaseItem,
         entityField: Field,
         typeArgElement: TypeElement
     ): List<String> {

stitchlv/compose-multiplatform-core

room/compiler/src/main/kotlin/androidx/room/processor/EntityProcessor.kt
@@ -28,8 +28,8 @@ import javax.lang.model.element.Name
 import javax.lang.model.element.TypeElement
 import javax.lang.model.type.TypeMirror
 
-interface EntityProcessor {
-    fun process(): androidx.room.vo.Entity
+interface EntityProcessor : EntityOrViewProcessor {
+    override fun process(): androidx.room.vo.Entity
 
     companion object {
         fun extractTableName(element: TypeElement, annotation: Entity): String {

room/compiler/src/main/kotlin/androidx/room/processor/PojoProcessor.kt
@@ -37,8 +37,8 @@ import androidx.room.processor.autovalue.AutoValuePojoProcessorDelegate
 import androidx.room.processor.cache.Cache
 import androidx.room.vo.CallType
 import androidx.room.vo.Constructor
+import androidx.room.vo.EntityOrView
 import androidx.room.vo.EmbeddedField
-import androidx.room.vo.Entity
 import androidx.room.vo.Field
 import androidx.room.vo.FieldGetter
 import androidx.room.vo.FieldSetter
@@ -499,7 +499,7 @@ class PojoProcessor private constructor(
             return null
         }
 
-        val entity = EntityProcessor(context, entityElement, referenceStack).process()
+        val entity = EntityOrViewProcessor(context, entityElement, referenceStack).process()
 
         // now find the field in the entity.
         val entityField = entity.fields.firstOrNull {
@@ -543,7 +543,7 @@ class PojoProcessor private constructor(
 
     private fun validateRelationshipProjection(
         projectionInput: Array<String>,
-        entity: Entity,
+        entity: EntityOrView,
         relationElement: VariableElement
     ) {
         val missingColumns = projectionInput.filterNot { columnName ->
@@ -568,7 +568,7 @@ class PojoProcessor private constructor(
     private fun createRelationshipProjection(
         inferEntity: Boolean,
         typeArg: TypeMirror,
-        entity: Entity,
+        entity: EntityOrView,
         entityField: Field,
         typeArgElement: TypeElement
     ): List<String> {