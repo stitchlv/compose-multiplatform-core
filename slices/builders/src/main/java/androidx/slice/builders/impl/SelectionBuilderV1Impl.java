aosp-mirror/platform_frameworks_support

Slices: Add SelectionBuilderV1Impl
This builder actually includes all the data necessary to render a
selection with options.

Test: coming in a later change once things are assembled
Change-Id: Id276d0767e55674ab6686bac64eab3cc19fd990d

slices/builders/src/main/java/androidx/slice/builders/impl/SelectionBuilderV1Impl.java
@@ -1,0 +1,84 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.slice.builders.impl;
+
+import java.util.List;
+
+import androidx.annotation.RequiresApi;
+import androidx.annotation.RestrictTo;
+import androidx.core.util.Pair;
+import androidx.slice.Slice;
+import androidx.slice.builders.SelectionBuilder;
+
+import static android.app.slice.Slice.HINT_TITLE;
+import static android.app.slice.Slice.SUBTYPE_CONTENT_DESCRIPTION;
+import static android.app.slice.Slice.SUBTYPE_LAYOUT_DIRECTION;
+import static androidx.annotation.RestrictTo.Scope.LIBRARY;
+import static androidx.slice.core.SliceHints.HINT_SELECTION;
+import static androidx.slice.core.SliceHints.HINT_SELECTION_OPTION;
+import static androidx.slice.core.SliceHints.SUBTYPE_SELECTION_OPTION_KEY;
+import static androidx.slice.core.SliceHints.SUBTYPE_SELECTION_SELECTED_OPTION_KEY;
+
+/**
+ * @hide
+ */
+@RestrictTo(LIBRARY)
+@RequiresApi(19)
+public class SelectionBuilderV1Impl extends SelectionBuilderImpl {
+    public SelectionBuilderV1Impl(Slice.Builder parentSliceBuilder,
+                                  SelectionBuilder selectionBuilder) {
+        super(parentSliceBuilder, selectionBuilder);
+    }
+
+    @Override
+    public void apply(SelectionBuilder selectionBuilder, Slice.Builder sliceBuilder) {
+        sliceBuilder.addHints(HINT_SELECTION);
+
+        selectionBuilder.getPrimaryAction().setPrimaryAction(sliceBuilder);
+
+        if (selectionBuilder.getTitle() != null) {
+            sliceBuilder.addText(selectionBuilder.getTitle(), null, HINT_TITLE);
+        }
+
+        if (selectionBuilder.getSubtitle() != null) {
+            sliceBuilder.addText(selectionBuilder.getSubtitle(), null);
+        }
+
+        if (selectionBuilder.getContentDescription() != null) {
+            sliceBuilder.addText(selectionBuilder.getContentDescription(),
+                    SUBTYPE_CONTENT_DESCRIPTION);
+        }
+
+        if (selectionBuilder.getLayoutDirection() != -1) {
+            sliceBuilder.addInt(selectionBuilder.getLayoutDirection(), SUBTYPE_LAYOUT_DIRECTION);
+        }
+
+        if (selectionBuilder.getSelectedOption() != null) {
+            sliceBuilder.addText(selectionBuilder.getSelectedOption(),
+                    SUBTYPE_SELECTION_SELECTED_OPTION_KEY);
+        }
+
+        final List<Pair<String, CharSequence>> options = selectionBuilder.getOptions();
+        for (Pair<String, CharSequence> option : options) {
+            final Slice.Builder optionSubSliceBuilder = new Slice.Builder(sliceBuilder);
+            optionSubSliceBuilder.addHints(HINT_SELECTION_OPTION);
+            optionSubSliceBuilder.addText(option.second, null);
+            optionSubSliceBuilder.addText(option.first, SUBTYPE_SELECTION_OPTION_KEY);
+            sliceBuilder.addSubSlice(optionSubSliceBuilder.build());
+        }
+    }
+}

stitchlv/compose-multiplatform-core

slices/builders/src/main/java/androidx/slice/builders/impl/SelectionBuilderV1Impl.java
@@ -1,0 +1,80 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.slice.builders.impl;
+
+import java.util.List;
+
+import androidx.annotation.RequiresApi;
+import androidx.annotation.RestrictTo;
+import androidx.core.util.Pair;
+import androidx.slice.Slice;
+import androidx.slice.builders.SelectionBuilder;
+
+import static android.app.slice.Slice.HINT_SELECTED;
+import static android.app.slice.Slice.HINT_TITLE;
+import static android.app.slice.Slice.SUBTYPE_CONTENT_DESCRIPTION;
+import static android.app.slice.Slice.SUBTYPE_LAYOUT_DIRECTION;
+import static androidx.annotation.RestrictTo.Scope.LIBRARY;
+import static androidx.slice.core.SliceHints.HINT_SELECTION;
+import static androidx.slice.core.SliceHints.SUBTYPE_SELECTION_OPTION_KEY;
+
+/**
+ * @hide
+ */
+@RestrictTo(LIBRARY)
+@RequiresApi(19)
+public class SelectionBuilderV1Impl extends SelectionBuilderImpl {
+    public SelectionBuilderV1Impl(Slice.Builder parentSliceBuilder,
+                                  SelectionBuilder selectionBuilder) {
+        super(parentSliceBuilder, selectionBuilder);
+    }
+
+    @Override
+    public void apply(SelectionBuilder selectionBuilder, Slice.Builder sliceBuilder) {
+        sliceBuilder.addHints(HINT_SELECTION);
+
+        selectionBuilder.getPrimaryAction().setPrimaryAction(sliceBuilder);
+
+        if (selectionBuilder.getTitle() != null) {
+            sliceBuilder.addText(selectionBuilder.getTitle(), null, HINT_TITLE);
+        }
+
+        if (selectionBuilder.getSubtitle() != null) {
+            sliceBuilder.addText(selectionBuilder.getSubtitle(), null);
+        }
+
+        if (selectionBuilder.getContentDescription() != null) {
+            sliceBuilder.addText(selectionBuilder.getContentDescription(),
+                    SUBTYPE_CONTENT_DESCRIPTION);
+        }
+
+        if (selectionBuilder.getLayoutDirection() != -1) {
+            sliceBuilder.addInt(selectionBuilder.getLayoutDirection(), SUBTYPE_LAYOUT_DIRECTION);
+        }
+
+        final List<Pair<String, CharSequence>> options = selectionBuilder.getOptions();
+        for (Pair<String, CharSequence> option : options) {
+            final Slice.Builder optionSubSliceBuilder = new Slice.Builder(sliceBuilder);
+            if (option.first.equals(selectionBuilder.getSelectedOption())) {
+                optionSubSliceBuilder.addHints(HINT_SELECTED);
+            }
+            optionSubSliceBuilder.addText(option.second, null);
+            optionSubSliceBuilder.addText(option.first, SUBTYPE_SELECTION_OPTION_KEY);
+            sliceBuilder.addSubSlice(optionSubSliceBuilder.build());
+        }
+    }
+}