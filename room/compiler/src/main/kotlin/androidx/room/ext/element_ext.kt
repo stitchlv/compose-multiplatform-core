aosp-mirror/platform_frameworks_support

Room Support for Coroutines
This change allows users to define suspend functions in DAOs with
@Query, @Insert, @Update and @Delete. The new artifact room-coroutine
defines a helper classes for supporting the feature.

Bug: 69474692
Test: ./gradlew room:integration-tests:kotlintestapp:cC
Change-Id: Id73a6b845df3f21f99d14cb6980130dcf3885c81

room/compiler/src/main/kotlin/androidx/room/ext/element_ext.kt
@@ -328,7 +328,7 @@ fun Types.isAssignableWithoutVariance(from: TypeMirror, to: TypeMirror): Boolean
 fun TypeMirror.extendsBound(): TypeMirror? {
     return this.accept(object : SimpleTypeVisitor7<TypeMirror?, Void?>() {
         override fun visitWildcard(type: WildcardType, ignored: Void?): TypeMirror? {
-            return type.extendsBound
+            return type.extendsBound ?: type.superBound
         }
     }, null)
 }
@@ -367,4 +367,16 @@ fun Element.findKotlinDefaultImpl(typeUtils: Types): Element? {
                 paramsMatch(MoreElements.asExecutable(this).parameters,
                         MoreElements.asExecutable(it).parameters)
     }
+}
+
+/**
+ * Finds the Kotlin's suspend function return type by inspecting the type param of the Continuation
+ * parameter of the function. This method assumes the executable element is a suspend function.
+ * @see KotlinMetadataElement.isSuspendFunction
+ */
+fun ExecutableElement.findKotlinSuspendFunctionReturnType(): TypeMirror {
+    // the continuation parameter is always the last parameter of a suspend function and it only has
+    // one type parameter, e.g Continuation<? super T>
+    val typeParam = MoreTypes.asDeclared(parameters.last().asType()).typeArguments.first()
+    return typeParam.extendsBoundOrSelf() // reduce the type param
 }

room/compiler/src/main/kotlin/androidx/room/ext/javapoet_ext.kt
@@ -153,6 +153,15 @@ object RoomRxJava2TypeNames {
             "EmptyResultSetException")
 }
 
+object RoomCoroutineTypeNames {
+    val COROUTINE_ROOM = ClassName.get("androidx.room", "CoroutineRoom")
+}
+
+object KotlinTypeNames {
+    val UNIT = ClassName.get("kotlin", "Unit")
+    val CONTINUATION = ClassName.get("kotlin.coroutines", "Continuation")
+}
+
 fun TypeName.defaultValue(): String {
     return if (!isPrimitive) {
         "null"

room/compiler/src/main/kotlin/androidx/room/solver/query/result/CoroutineResultBinder.kt
@@ -1,0 +1,110 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.room.solver.query.result
+
+import androidx.room.ext.AndroidTypeNames
+import androidx.room.ext.CallableTypeSpecBuilder
+import androidx.room.ext.L
+import androidx.room.ext.N
+import androidx.room.ext.RoomCoroutineTypeNames
+import androidx.room.ext.RoomTypeNames
+import androidx.room.ext.T
+import androidx.room.ext.typeName
+import androidx.room.solver.CodeGenScope
+import com.squareup.javapoet.FieldSpec
+import com.squareup.javapoet.MethodSpec
+import javax.lang.model.type.TypeMirror
+
+/**
+ * Binds the result of a of a Kotlin coroutine suspend function.
+ */
+class CoroutineResultBinder(
+    val typeArg: TypeMirror,
+    private val continuationParamName: String,
+    adapter: QueryResultAdapter?
+) : QueryResultBinder(adapter) {
+
+    override fun convertAndReturn(
+        roomSQLiteQueryVar: String,
+        canReleaseQuery: Boolean,
+        dbField: FieldSpec,
+        inTransaction: Boolean,
+        scope: CodeGenScope
+    ) {
+        val callableImpl = CallableTypeSpecBuilder(typeArg.typeName()) {
+            createRunQueryAndReturnStatements(
+                builder = this,
+                roomSQLiteQueryVar = roomSQLiteQueryVar,
+                canReleaseQuery = canReleaseQuery,
+                dbField = dbField,
+                inTransaction = inTransaction,
+                scope = scope)
+        }.build()
+
+        scope.builder().apply {
+            addStatement(
+                "return $T.execute($N, $L, $N)",
+                RoomCoroutineTypeNames.COROUTINE_ROOM,
+                dbField,
+                callableImpl,
+                continuationParamName)
+        }
+    }
+
+    private fun createRunQueryAndReturnStatements(
+        builder: MethodSpec.Builder,
+        roomSQLiteQueryVar: String,
+        canReleaseQuery: Boolean,
+        dbField: FieldSpec,
+        inTransaction: Boolean,
+        scope: CodeGenScope
+    ) {
+        val transactionWrapper = if (inTransaction) {
+            builder.transactionWrapper(dbField)
+        } else {
+            null
+        }
+        val shouldCopyCursor = adapter?.shouldCopyCursor() == true
+        val outVar = scope.getTmpVar("_result")
+        val cursorVar = scope.getTmpVar("_cursor")
+        transactionWrapper?.beginTransactionWithControlFlow()
+        builder.apply {
+            addStatement("final $T $L = $T.query($N, $L, $L)",
+                AndroidTypeNames.CURSOR,
+                cursorVar,
+                RoomTypeNames.DB_UTIL,
+                dbField,
+                roomSQLiteQueryVar,
+                if (shouldCopyCursor) "true" else "false")
+            beginControlFlow("try").apply {
+                val adapterScope = scope.fork()
+                adapter?.convert(outVar, cursorVar, adapterScope)
+                addCode(adapterScope.builder().build())
+                transactionWrapper?.commitTransaction()
+                addStatement("return $L", outVar)
+            }
+            nextControlFlow("finally").apply {
+                addStatement("$L.close()", cursorVar)
+                if (canReleaseQuery) {
+                    addStatement("$L.release()", roomSQLiteQueryVar)
+                }
+            }
+            endControlFlow()
+        }
+        transactionWrapper?.endTransactionWithControlFlow()
+    }
+}

room/compiler/src/main/kotlin/androidx/room/solver/shortcut/binder/CoroutineDeleteOrUpdateMethodBinder.kt
@@ -1,0 +1,69 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.room.solver.shortcut.binder
+
+import androidx.room.ext.CallableTypeSpecBuilder
+import androidx.room.ext.L
+import androidx.room.ext.N
+import androidx.room.ext.RoomCoroutineTypeNames
+import androidx.room.ext.T
+import androidx.room.ext.typeName
+import androidx.room.solver.CodeGenScope
+import androidx.room.solver.shortcut.result.DeleteOrUpdateMethodAdapter
+import androidx.room.vo.ShortcutQueryParameter
+import androidx.room.writer.DaoWriter
+import com.squareup.javapoet.FieldSpec
+import com.squareup.javapoet.TypeSpec
+import javax.lang.model.type.TypeMirror
+
+/**
+ * Binder that knows how to write delete and update methods that are suspend functions.
+ */
+class CoroutineDeleteOrUpdateMethodBinder(
+    private val typeArg: TypeMirror,
+    private val continuationParamName: String,
+    adapter: DeleteOrUpdateMethodAdapter?
+
+) : DeleteOrUpdateMethodBinder(adapter) {
+
+    private val instantDeleteOrUpdateMethodBinder = InstantDeleteOrUpdateMethodBinder(adapter)
+
+    override fun convertAndReturn(
+        parameters: List<ShortcutQueryParameter>,
+        adapters: Map<String, Pair<FieldSpec, TypeSpec>>,
+        scope: CodeGenScope
+    ) {
+        val adapterScope = scope.fork()
+        val callableImpl = CallableTypeSpecBuilder(typeArg.typeName()) {
+            instantDeleteOrUpdateMethodBinder.convertAndReturn(
+                parameters = parameters,
+                adapters = adapters,
+                scope = adapterScope
+            )
+            addCode(adapterScope.generate())
+        }.build()
+
+        scope.builder().apply {
+            addStatement(
+                "return $T.execute($N, $L, $N)",
+                RoomCoroutineTypeNames.COROUTINE_ROOM,
+                DaoWriter.dbField,
+                callableImpl,
+                continuationParamName)
+        }
+    }
+}

room/compiler/src/main/kotlin/androidx/room/solver/shortcut/binder/CoroutineInsertMethodBinder.kt
@@ -1,0 +1,69 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.room.solver.shortcut.binder
+
+import androidx.room.ext.CallableTypeSpecBuilder
+import androidx.room.ext.L
+import androidx.room.ext.N
+import androidx.room.ext.RoomCoroutineTypeNames
+import androidx.room.ext.T
+import androidx.room.ext.typeName
+import androidx.room.solver.CodeGenScope
+import androidx.room.solver.shortcut.result.InsertMethodAdapter
+import androidx.room.vo.ShortcutQueryParameter
+import androidx.room.writer.DaoWriter
+import com.squareup.javapoet.FieldSpec
+import com.squareup.javapoet.TypeSpec
+import javax.lang.model.type.TypeMirror
+
+/**
+ * Binder that knows how to write insert methods that are suspend functions.
+ */
+class CoroutineInsertMethodBinder(
+    val typeArg: TypeMirror,
+    private val continuationParamName: String,
+    adapter: InsertMethodAdapter?
+) : InsertMethodBinder(adapter) {
+
+    private val instantInsertMethodBinder = InstantInsertMethodBinder(adapter)
+
+    override fun convertAndReturn(
+        parameters: List<ShortcutQueryParameter>,
+        insertionAdapters: Map<String, Pair<FieldSpec, TypeSpec>>,
+        scope: CodeGenScope
+    ) {
+
+        val adapterScope = scope.fork()
+        val callableImpl = CallableTypeSpecBuilder(typeArg.typeName()) {
+            instantInsertMethodBinder.convertAndReturn(
+                parameters = parameters,
+                insertionAdapters = insertionAdapters,
+                scope = adapterScope
+            )
+            addCode(adapterScope.generate())
+        }.build()
+
+        scope.builder().apply {
+            addStatement(
+                "return $T.execute($N, $L, $N)",
+                RoomCoroutineTypeNames.COROUTINE_ROOM,
+                DaoWriter.dbField,
+                callableImpl,
+                continuationParamName)
+        }
+    }
+}

stitchlv/compose-multiplatform-core

room/compiler/src/main/kotlin/androidx/room/ext/element_ext.kt
@@ -328,7 +328,7 @@ fun Types.isAssignableWithoutVariance(from: TypeMirror, to: TypeMirror): Boolean
 fun TypeMirror.extendsBound(): TypeMirror? {
     return this.accept(object : SimpleTypeVisitor7<TypeMirror?, Void?>() {
         override fun visitWildcard(type: WildcardType, ignored: Void?): TypeMirror? {
-            return type.extendsBound
+            return type.extendsBound ?: type.superBound
         }
     }, null)
 }
@@ -367,4 +367,16 @@ fun Element.findKotlinDefaultImpl(typeUtils: Types): Element? {
                 paramsMatch(MoreElements.asExecutable(this).parameters,
                         MoreElements.asExecutable(it).parameters)
     }
+}
+
+/**
+ * Finds the Kotlin's suspend function return type by inspecting the type param of the Continuation
+ * parameter of the function. This method assumes the executable element is a suspend function.
+ * @see KotlinMetadataElement.isSuspendFunction
+ */
+fun ExecutableElement.getSuspendFunctionReturnType(): TypeMirror {
+    // the continuation parameter is always the last parameter of a suspend function and it only has
+    // one type parameter, e.g Continuation<? super T>
+    val typeParam = MoreTypes.asDeclared(parameters.last().asType()).typeArguments.first()
+    return typeParam.extendsBoundOrSelf() // reduce the type param
 }

room/compiler/src/main/kotlin/androidx/room/ext/javapoet_ext.kt
@@ -153,6 +153,15 @@ object RoomRxJava2TypeNames {
             "EmptyResultSetException")
 }
 
+object RoomCoroutinesTypeNames {
+    val COROUTINES_ROOM = ClassName.get("androidx.room", "CoroutinesRoom")
+}
+
+object KotlinTypeNames {
+    val UNIT = ClassName.get("kotlin", "Unit")
+    val CONTINUATION = ClassName.get("kotlin.coroutines", "Continuation")
+}
+
 fun TypeName.defaultValue(): String {
     return if (!isPrimitive) {
         "null"

room/compiler/src/main/kotlin/androidx/room/solver/query/result/CoroutineResultBinder.kt
@@ -1,0 +1,110 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.room.solver.query.result
+
+import androidx.room.ext.AndroidTypeNames
+import androidx.room.ext.CallableTypeSpecBuilder
+import androidx.room.ext.L
+import androidx.room.ext.N
+import androidx.room.ext.RoomCoroutinesTypeNames
+import androidx.room.ext.RoomTypeNames
+import androidx.room.ext.T
+import androidx.room.ext.typeName
+import androidx.room.solver.CodeGenScope
+import com.squareup.javapoet.FieldSpec
+import com.squareup.javapoet.MethodSpec
+import javax.lang.model.type.TypeMirror
+
+/**
+ * Binds the result of a of a Kotlin coroutine suspend function.
+ */
+class CoroutineResultBinder(
+    val typeArg: TypeMirror,
+    private val continuationParamName: String,
+    adapter: QueryResultAdapter?
+) : QueryResultBinder(adapter) {
+
+    override fun convertAndReturn(
+        roomSQLiteQueryVar: String,
+        canReleaseQuery: Boolean,
+        dbField: FieldSpec,
+        inTransaction: Boolean,
+        scope: CodeGenScope
+    ) {
+        val callableImpl = CallableTypeSpecBuilder(typeArg.typeName()) {
+            createRunQueryAndReturnStatements(
+                builder = this,
+                roomSQLiteQueryVar = roomSQLiteQueryVar,
+                canReleaseQuery = canReleaseQuery,
+                dbField = dbField,
+                inTransaction = inTransaction,
+                scope = scope)
+        }.build()
+
+        scope.builder().apply {
+            addStatement(
+                "return $T.execute($N, $L, $N)",
+                RoomCoroutinesTypeNames.COROUTINES_ROOM,
+                dbField,
+                callableImpl,
+                continuationParamName)
+        }
+    }
+
+    private fun createRunQueryAndReturnStatements(
+        builder: MethodSpec.Builder,
+        roomSQLiteQueryVar: String,
+        canReleaseQuery: Boolean,
+        dbField: FieldSpec,
+        inTransaction: Boolean,
+        scope: CodeGenScope
+    ) {
+        val transactionWrapper = if (inTransaction) {
+            builder.transactionWrapper(dbField)
+        } else {
+            null
+        }
+        val shouldCopyCursor = adapter?.shouldCopyCursor() == true
+        val outVar = scope.getTmpVar("_result")
+        val cursorVar = scope.getTmpVar("_cursor")
+        transactionWrapper?.beginTransactionWithControlFlow()
+        builder.apply {
+            addStatement("final $T $L = $T.query($N, $L, $L)",
+                AndroidTypeNames.CURSOR,
+                cursorVar,
+                RoomTypeNames.DB_UTIL,
+                dbField,
+                roomSQLiteQueryVar,
+                if (shouldCopyCursor) "true" else "false")
+            beginControlFlow("try").apply {
+                val adapterScope = scope.fork()
+                adapter?.convert(outVar, cursorVar, adapterScope)
+                addCode(adapterScope.builder().build())
+                transactionWrapper?.commitTransaction()
+                addStatement("return $L", outVar)
+            }
+            nextControlFlow("finally").apply {
+                addStatement("$L.close()", cursorVar)
+                if (canReleaseQuery) {
+                    addStatement("$L.release()", roomSQLiteQueryVar)
+                }
+            }
+            endControlFlow()
+        }
+        transactionWrapper?.endTransactionWithControlFlow()
+    }
+}

room/compiler/src/main/kotlin/androidx/room/solver/shortcut/binder/CoroutineDeleteOrUpdateMethodBinder.kt
@@ -1,0 +1,69 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.room.solver.shortcut.binder
+
+import androidx.room.ext.CallableTypeSpecBuilder
+import androidx.room.ext.L
+import androidx.room.ext.N
+import androidx.room.ext.RoomCoroutinesTypeNames
+import androidx.room.ext.T
+import androidx.room.ext.typeName
+import androidx.room.solver.CodeGenScope
+import androidx.room.solver.shortcut.result.DeleteOrUpdateMethodAdapter
+import androidx.room.vo.ShortcutQueryParameter
+import androidx.room.writer.DaoWriter
+import com.squareup.javapoet.FieldSpec
+import com.squareup.javapoet.TypeSpec
+import javax.lang.model.type.TypeMirror
+
+/**
+ * Binder that knows how to write delete and update methods that are suspend functions.
+ */
+class CoroutineDeleteOrUpdateMethodBinder(
+    private val typeArg: TypeMirror,
+    private val continuationParamName: String,
+    adapter: DeleteOrUpdateMethodAdapter?
+
+) : DeleteOrUpdateMethodBinder(adapter) {
+
+    private val instantDeleteOrUpdateMethodBinder = InstantDeleteOrUpdateMethodBinder(adapter)
+
+    override fun convertAndReturn(
+        parameters: List<ShortcutQueryParameter>,
+        adapters: Map<String, Pair<FieldSpec, TypeSpec>>,
+        scope: CodeGenScope
+    ) {
+        val adapterScope = scope.fork()
+        val callableImpl = CallableTypeSpecBuilder(typeArg.typeName()) {
+            instantDeleteOrUpdateMethodBinder.convertAndReturn(
+                parameters = parameters,
+                adapters = adapters,
+                scope = adapterScope
+            )
+            addCode(adapterScope.generate())
+        }.build()
+
+        scope.builder().apply {
+            addStatement(
+                "return $T.execute($N, $L, $N)",
+                RoomCoroutinesTypeNames.COROUTINES_ROOM,
+                DaoWriter.dbField,
+                callableImpl,
+                continuationParamName)
+        }
+    }
+}

room/compiler/src/main/kotlin/androidx/room/solver/shortcut/binder/CoroutineInsertMethodBinder.kt
@@ -1,0 +1,69 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.room.solver.shortcut.binder
+
+import androidx.room.ext.CallableTypeSpecBuilder
+import androidx.room.ext.L
+import androidx.room.ext.N
+import androidx.room.ext.RoomCoroutinesTypeNames
+import androidx.room.ext.T
+import androidx.room.ext.typeName
+import androidx.room.solver.CodeGenScope
+import androidx.room.solver.shortcut.result.InsertMethodAdapter
+import androidx.room.vo.ShortcutQueryParameter
+import androidx.room.writer.DaoWriter
+import com.squareup.javapoet.FieldSpec
+import com.squareup.javapoet.TypeSpec
+import javax.lang.model.type.TypeMirror
+
+/**
+ * Binder that knows how to write insert methods that are suspend functions.
+ */
+class CoroutineInsertMethodBinder(
+    val typeArg: TypeMirror,
+    private val continuationParamName: String,
+    adapter: InsertMethodAdapter?
+) : InsertMethodBinder(adapter) {
+
+    private val instantInsertMethodBinder = InstantInsertMethodBinder(adapter)
+
+    override fun convertAndReturn(
+        parameters: List<ShortcutQueryParameter>,
+        insertionAdapters: Map<String, Pair<FieldSpec, TypeSpec>>,
+        scope: CodeGenScope
+    ) {
+
+        val adapterScope = scope.fork()
+        val callableImpl = CallableTypeSpecBuilder(typeArg.typeName()) {
+            instantInsertMethodBinder.convertAndReturn(
+                parameters = parameters,
+                insertionAdapters = insertionAdapters,
+                scope = adapterScope
+            )
+            addCode(adapterScope.generate())
+        }.build()
+
+        scope.builder().apply {
+            addStatement(
+                "return $T.execute($N, $L, $N)",
+                RoomCoroutinesTypeNames.COROUTINES_ROOM,
+                DaoWriter.dbField,
+                callableImpl,
+                continuationParamName)
+        }
+    }
+}