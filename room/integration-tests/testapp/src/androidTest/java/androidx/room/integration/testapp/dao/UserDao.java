aosp-mirror/platform_frameworks_support

Allow Collection as a parameter of IN
Test: SimpleEntityReadWriteTest.queryByCollection
Bug: 119884035
Change-Id: Ia1e6b3e57e706fb717c910d04f6938311d703d8b

room/integration-tests/testapp/src/androidTest/java/androidx/room/integration/testapp/dao/UserDao.java
@@ -40,6 +40,7 @@ import com.google.common.util.concurrent.ListenableFuture;
 
 import org.reactivestreams.Publisher;
 
+import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 import java.util.Set;
@@ -69,6 +70,9 @@ public abstract class UserDao {
 
     @Query("select * from user where mId IN(:ids)")
     public abstract User[] loadByIds(int... ids);
+
+    @Query("select * from user where mId IN(:ids)")
+    public abstract List<User> loadByIds(Collection<Integer> ids);
 
     @Query("select * from user where custommm = :customField")
     public abstract List<User> findByCustomField(String customField);

stitchlv/compose-multiplatform-core

room/integration-tests/testapp/src/androidTest/java/androidx/room/integration/testapp/dao/UserDao.java
@@ -40,8 +40,10 @@ import com.google.common.util.concurrent.ListenableFuture;
 
 import org.reactivestreams.Publisher;
 
+import java.util.Collection;
 import java.util.Date;
 import java.util.List;
+import java.util.Queue;
 import java.util.Set;
 import java.util.concurrent.Callable;
 
@@ -69,6 +71,15 @@ public abstract class UserDao {
 
     @Query("select * from user where mId IN(:ids)")
     public abstract User[] loadByIds(int... ids);
+
+    @Query("select * from user where mId IN(:ids)")
+    public abstract List<User> loadByIdCollection(Collection<Integer> ids);
+
+    @Query("select * from user where mId IN(:ids)")
+    public abstract List<User> loadByIdSet(Set<Integer> ids);
+
+    @Query("select * from user where mId IN(:ids)")
+    public abstract List<User> loadByIdQueue(Queue<Integer> ids);
 
     @Query("select * from user where custommm = :customField")
     public abstract List<User> findByCustomField(String customField);