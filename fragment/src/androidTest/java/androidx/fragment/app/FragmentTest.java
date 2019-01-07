aosp-mirror/platform_frameworks_support

Add requireArguments() to Fragment
In cases where you know the Fragment has
arguments, provide a requireArguments()
alternative to using arguments!! in Kotlin.

Test: new FragmentTest
BUG: 121196360
Change-Id: Ia73fb9f0ca283109880e3941a61ef9e39c6a5d7e

fragment/src/androidTest/java/androidx/fragment/app/FragmentTest.java
@@ -16,6 +16,7 @@
 package androidx.fragment.app;
 
 import static com.google.common.truth.Truth.assertThat;
+import static com.google.common.truth.Truth.assertWithMessage;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -266,6 +267,22 @@ public class FragmentTest {
             fail();
         } catch (IllegalStateException expected) {
         }
+    }
+
+    @SmallTest
+    @Test
+    public void requireArguments() {
+        Fragment fragment = new Fragment();
+        try {
+            fragment.requireArguments();
+            fail();
+        } catch (IllegalStateException expected) {
+        }
+        Bundle arguments = new Bundle();
+        fragment.setArguments(arguments);
+        assertWithMessage("requireArguments should return the arguments")
+                .that(fragment.requireArguments())
+                .isEqualTo(arguments);
     }
 
     public static class OrderFragment extends Fragment {

stitchlv/compose-multiplatform-core

fragment/src/androidTest/java/androidx/fragment/app/FragmentTest.java
@@ -16,6 +16,7 @@
 package androidx.fragment.app;
 
 import static com.google.common.truth.Truth.assertThat;
+import static com.google.common.truth.Truth.assertWithMessage;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -266,6 +267,22 @@ public class FragmentTest {
             fail();
         } catch (IllegalStateException expected) {
         }
+    }
+
+    @SmallTest
+    @Test
+    public void requireArguments() {
+        Fragment fragment = new Fragment();
+        try {
+            fragment.requireArguments();
+            fail();
+        } catch (IllegalStateException expected) {
+        }
+        Bundle arguments = new Bundle();
+        fragment.setArguments(arguments);
+        assertWithMessage("requireArguments should return the arguments")
+                .that(fragment.requireArguments())
+                .isSameAs(arguments);
     }
 
     public static class OrderFragment extends Fragment {