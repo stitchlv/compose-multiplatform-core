aosp-mirror/platform_frameworks_support

Add attributes to customize slice row style
- Add new attributes to customize paddings and divider height of row view
- seperate headerDividerPadding into bottomDividerStartPadding and bottomDividerEndPadding

Test: ./gradlew slice-view:connectedCheck
Change-Id: I2cdf563fcada8944d9735a46fe5baeb2a0b1479c
Fixes: 114807643

slices/view/src/main/java/androidx/slice/widget/RowView.java
@@ -156,7 +156,6 @@ public class RowView extends SliceChildView implements View.OnClickListener {
     private int mIdealRangeHeight;
     // How big mRangeBar wants to be.
     private int mMeasuredRangeHeight;
-    private int mMaxSmallHeight;
 
     public RowView(Context context) {
         super(context);
@@ -180,6 +179,56 @@ public class RowView extends SliceChildView implements View.OnClickListener {
 
         mIdealRangeHeight = context.getResources().getDimensionPixelSize(
                 R.dimen.abc_slice_row_range_height);
+    }
+
+    @Override
+    public void setStyle(SliceStyle styles) {
+        super.setStyle(styles);
+        applyRowStyle();
+    }
+
+    private void applyRowStyle() {
+        if (mSliceStyle == null || mSliceStyle.getRowStyle() == null) {
+            return;
+        }
+
+        final RowStyle rowStyle = mSliceStyle.getRowStyle();
+        setViewPaddingEnd(mStartContainer, rowStyle.getIconFrameEndPadding());
+        setViewSidePaddings(mContent,
+                rowStyle.getContentStartPadding(), rowStyle.getContentEndPadding());
+        setViewSidePaddings(mEndContainer,
+                rowStyle.getWidgetFrameStartPadding(), rowStyle.getWidgetFrameEndPadding());
+        setViewSideMargins(mBottomDivider,
+                rowStyle.getBottomDividerStartPadding(), rowStyle.getBottomDividerEndPadding());
+        setViewHeight(mActionDivider, rowStyle.getActionDividerHeight());
+    }
+
+    private void setViewPaddingEnd(View v, int end) {
+        if (v != null && end >= 0) {
+            v.setPaddingRelative(v.getPaddingStart(), v.getPaddingTop(), end, v.getPaddingBottom());
+        }
+    }
+
+    private void setViewSidePaddings(View v, int start, int end) {
+        if (v != null && start >= 0 && end >= 0) {
+            v.setPaddingRelative(start, v.getPaddingTop(), end, v.getPaddingBottom());
+        }
+    }
+
+    private void setViewSideMargins(View v, int start, int end) {
+        if (v != null && start >= 0 && end >= 0) {
+            final MarginLayoutParams params = (MarginLayoutParams) v.getLayoutParams();
+            params.setMargins(start, 0, end, 0);
+            mBottomDivider.setLayoutParams(params);
+        }
+    }
+
+    private void setViewHeight(View v, int height) {
+        if (v != null && height >= 0) {
+            final ViewGroup.LayoutParams params = v.getLayoutParams();
+            params.height = height;
+            v.setLayoutParams(params);
+        }
     }
 
     @Override
@@ -348,18 +397,7 @@ public class RowView extends SliceChildView implements View.OnClickListener {
 
         addSubtitle(titleItem != null /* hasTitle */);
 
-        if (mRowContent.hasBottomDivider()) {
-            if (mSliceStyle != null) {
-                final int padding = mSliceStyle.getHeaderDividerPadding();
-                LinearLayout.LayoutParams lp =
-                        (LinearLayout.LayoutParams) mBottomDivider.getLayoutParams();
-                lp.setMargins(padding, 0, padding, 0);
-                mBottomDivider.setLayoutParams(lp);
-            }
-            mBottomDivider.setVisibility(View.VISIBLE);
-        } else {
-            mBottomDivider.setVisibility(View.GONE);
-        }
+        mBottomDivider.setVisibility(mRowContent.hasBottomDivider() ? View.VISIBLE : View.GONE);
 
         SliceItem primaryAction = mRowContent.getPrimaryAction();
         if (primaryAction != null && primaryAction != mStartItem) {

stitchlv/compose-multiplatform-core

Bug: 114807643

slices/view/src/main/java/androidx/slice/widget/RowView.java
@@ -156,7 +156,6 @@ public class RowView extends SliceChildView implements View.OnClickListener {
     private int mIdealRangeHeight;
     // How big mRangeBar wants to be.
     private int mMeasuredRangeHeight;
-    private int mMaxSmallHeight;
 
     public RowView(Context context) {
         super(context);
@@ -180,6 +179,57 @@ public class RowView extends SliceChildView implements View.OnClickListener {
 
         mIdealRangeHeight = context.getResources().getDimensionPixelSize(
                 R.dimen.abc_slice_row_range_height);
+    }
+
+    @Override
+    public void setStyle(SliceStyle styles) {
+        super.setStyle(styles);
+        applyRowStyle();
+    }
+
+    private void applyRowStyle() {
+        if (mSliceStyle == null || mSliceStyle.getRowStyle() == null) {
+            return;
+        }
+
+        final RowStyle rowStyle = mSliceStyle.getRowStyle();
+        setViewPaddingEnd(mStartContainer, rowStyle.getTitleItemEndPadding());
+        setViewSidePaddings(mContent,
+                rowStyle.getContentStartPadding(), rowStyle.getContentEndPadding());
+        setViewSidePaddings(mEndContainer,
+                rowStyle.getEndItemStartPadding(), rowStyle.getEndItemEndPadding());
+        setViewSideMargins(mBottomDivider,
+                rowStyle.getBottomDividerStartPadding(), rowStyle.getBottomDividerEndPadding());
+        setViewHeight(mActionDivider, rowStyle.getActionDividerHeight());
+    }
+
+    private void setViewPaddingEnd(View v, int end) {
+        if (v != null && end >= 0) {
+            v.setPaddingRelative(v.getPaddingStart(), v.getPaddingTop(), end, v.getPaddingBottom());
+        }
+    }
+
+    private void setViewSidePaddings(View v, int start, int end) {
+        if (v != null && start >= 0 && end >= 0) {
+            v.setPaddingRelative(start, v.getPaddingTop(), end, v.getPaddingBottom());
+        }
+    }
+
+    private void setViewSideMargins(View v, int start, int end) {
+        if (v != null && start >= 0 && end >= 0) {
+            final MarginLayoutParams params = (MarginLayoutParams) v.getLayoutParams();
+            params.setMarginStart(start);
+            params.setMarginEnd(end);
+            mBottomDivider.setLayoutParams(params);
+        }
+    }
+
+    private void setViewHeight(View v, int height) {
+        if (v != null && height >= 0) {
+            final ViewGroup.LayoutParams params = v.getLayoutParams();
+            params.height = height;
+            v.setLayoutParams(params);
+        }
     }
 
     @Override
@@ -348,18 +398,7 @@ public class RowView extends SliceChildView implements View.OnClickListener {
 
         addSubtitle(titleItem != null /* hasTitle */);
 
-        if (mRowContent.hasBottomDivider()) {
-            if (mSliceStyle != null) {
-                final int padding = mSliceStyle.getHeaderDividerPadding();
-                LinearLayout.LayoutParams lp =
-                        (LinearLayout.LayoutParams) mBottomDivider.getLayoutParams();
-                lp.setMargins(padding, 0, padding, 0);
-                mBottomDivider.setLayoutParams(lp);
-            }
-            mBottomDivider.setVisibility(View.VISIBLE);
-        } else {
-            mBottomDivider.setVisibility(View.GONE);
-        }
+        mBottomDivider.setVisibility(mRowContent.hasBottomDivider() ? View.VISIBLE : View.GONE);
 
         SliceItem primaryAction = mRowContent.getPrimaryAction();
         if (primaryAction != null && primaryAction != mStartItem) {