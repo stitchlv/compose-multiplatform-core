aosp-mirror/platform_frameworks_support

Change API to avoid auto-complete pollution
The ktx extension API that was previous defined for creating spring
and fling animations are applicable to all objects, which results
in auto-complete polution for whoever uses this lib. This CL
modified the method signature not only to fix this issue, but also
improves the usability of these APIs

BUG: 127327788
Test: included in the CL

Change-Id: I51c257fb03e1bfeb1895ae59a2ac7dd8e6f084c8

dynamic-animation/ktx/src/androidTest/java/androidx/dynamicanimation/tests/DynamicAnimationTest.kt
@@ -1,0 +1,138 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.dynamicanimation.tests
+
+import android.view.View
+
+import androidx.dynamicanimation.animation.DynamicAnimation
+import androidx.dynamicanimation.animation.SpringForce
+import androidx.dynamicanimation.animation.flingAnimationOfProperty
+import androidx.dynamicanimation.animation.springAnimationOfProperty
+import androidx.dynamicanimation.animation.withSpringForceProperties
+import androidx.dynamicanimation.ktx.test.R
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.rule.ActivityTestRule
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertNull
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.timeout
+import org.mockito.Mockito.verify
+
+@SmallTest
+@RunWith(AndroidJUnit4::class)
+class DynamicAnimationTest {
+
+    @get:Rule val activityTestRule = ActivityTestRule<AnimationActivity>(
+        AnimationActivity::class.java)
+
+    private lateinit var view: View
+
+    @Before fun setup() {
+        view = activityTestRule.getActivity().findViewById(R.id.anim_view)
+    }
+
+    /**
+     * Test extension for creating [FlingAnimation]
+     */
+    @Test fun testCreateFlingAnimation() {
+        val flingAnimation = flingAnimationOfProperty(view::setAlpha, view::getAlpha)
+        assertNotNull(flingAnimation)
+        assertTrue(flingAnimation.friction == 1f)
+        flingAnimation.friction = 1.5f
+        assertTrue(flingAnimation.friction == 1.5f)
+    }
+
+    /**
+     * Test extension for creating [SpringAnimation]
+     */
+    @Test fun testCreateSpringAnimation() {
+        val springAnimationWithoutFinalPosition =
+            springAnimationOfProperty(view::setScaleX, view::getScaleX)
+        assertNotNull(springAnimationWithoutFinalPosition)
+        assertNull(springAnimationWithoutFinalPosition.spring)
+        val springAnimationWithFinalPosition =
+            springAnimationOfProperty(view::setScaleX, view::getScaleX, 100f)
+        assertNotNull(springAnimationWithFinalPosition)
+        assertNotNull(springAnimationWithFinalPosition.spring)
+        assertEquals(springAnimationWithFinalPosition.spring.finalPosition, 100f)
+
+        val listener: DynamicAnimation.OnAnimationEndListener = mock(
+                DynamicAnimation.OnAnimationEndListener::class.java)
+        springAnimationWithFinalPosition.addEndListener(listener)
+        InstrumentationRegistry.getInstrumentation().runOnMainSync({
+                springAnimationWithFinalPosition.setStartValue(0f).start()
+        })
+        assertTrue(springAnimationWithFinalPosition.isRunning())
+
+        InstrumentationRegistry.getInstrumentation().runOnMainSync({
+                springAnimationWithFinalPosition.skipToEnd()
+        })
+
+        verify(listener, timeout(2000)).onAnimationEnd(springAnimationWithFinalPosition, false,
+            100f, 0f)
+
+        assertEquals(100f, view.getScaleX())
+    }
+
+    /**
+     * Test extension for setting up [SpringForce]
+     */
+    @Test fun testSpringForce() {
+        val springAnimationWithoutFinalPosition = springAnimationOfProperty(
+            view::setTranslationX, view::getTranslationX)
+            .withSpringForceProperties {
+                finalPosition = 100f
+                dampingRatio = SpringForce.DAMPING_RATIO_HIGH_BOUNCY
+                stiffness = SpringForce.STIFFNESS_HIGH
+            }
+        assertNotNull(springAnimationWithoutFinalPosition.spring)
+        assertEquals(springAnimationWithoutFinalPosition.spring.finalPosition, 100f)
+        assertEquals(
+            springAnimationWithoutFinalPosition.spring.dampingRatio,
+            SpringForce.DAMPING_RATIO_HIGH_BOUNCY
+        )
+        assertEquals(
+            springAnimationWithoutFinalPosition.spring.stiffness,
+            SpringForce.STIFFNESS_HIGH
+        )
+        val springAnimationWithFinalPosition = springAnimationOfProperty(
+            view::setTranslationX, view::getTranslationX, 120f)
+            .withSpringForceProperties {
+                dampingRatio = SpringForce.DAMPING_RATIO_LOW_BOUNCY
+                stiffness = SpringForce.STIFFNESS_LOW
+            }
+        assertNotNull(springAnimationWithFinalPosition.spring)
+        assertEquals(springAnimationWithFinalPosition.spring.finalPosition, 120f)
+        assertEquals(
+            springAnimationWithFinalPosition.spring.dampingRatio,
+            SpringForce.DAMPING_RATIO_LOW_BOUNCY
+        )
+        assertEquals(
+            springAnimationWithFinalPosition.spring.stiffness,
+            SpringForce.STIFFNESS_LOW
+        )
+    }
+}

dynamic-animation/ktx/src/main/java/androidx/dynamicanimation/animation/DynamicAnimation.kt
@@ -17,32 +17,36 @@
 package androidx.dynamicanimation.animation
 
 /**
- * Creates [FlingAnimation] for object.
+ * Creates [FlingAnimation] for a property that can be accessed via the provided setter and getter.
  *
- * @param property object's property to be animated.
+ * @param setter The function that mutates the property being animated
+ * @param getter The function that returns the value of the property
  * @return [FlingAnimation]
  */
-inline fun <K> K.flingAnimationOf(property: FloatPropertyCompat<K>): FlingAnimation {
-    return FlingAnimation(this, property)
+fun flingAnimationOfProperty(setter: (Float) -> Unit, getter: () -> Float): FlingAnimation {
+    return FlingAnimation(createFloatValueHolder(setter, getter))
 }
 
 /**
- * Creates [SpringAnimation] for object.
+ * Creates [SpringAnimation] for a property that can be accessed via the provided setter and getter.
  * If finalPosition is not [Float.NaN] then create [SpringAnimation] with
  * [SpringForce.mFinalPosition].
  *
- * @param property object's property to be animated.
+ * @param setter The function that mutates the property being animated
+ * @param getter The function that returns the value of the property
  * @param finalPosition [SpringForce.mFinalPosition] Final position of spring.
  * @return [SpringAnimation]
  */
-inline fun <K> K.springAnimationOf(
-    property: FloatPropertyCompat<K>,
+fun springAnimationOfProperty(
+    setter: (Float) -> Unit,
+    getter: () -> Float,
     finalPosition: Float = Float.NaN
 ): SpringAnimation {
+    val valueHolder = createFloatValueHolder(setter, getter)
     return if (finalPosition.isNaN()) {
-        SpringAnimation(this, property)
+        SpringAnimation(valueHolder)
     } else {
-        SpringAnimation(this, property, finalPosition)
+        SpringAnimation(valueHolder, finalPosition)
     }
 }
 
@@ -64,4 +68,16 @@ inline fun SpringAnimation.withSpringForceProperties(
     }
     spring.func()
     return this
+}
+
+private fun createFloatValueHolder(setter: (Float) -> Unit, getter: () -> Float): FloatValueHolder {
+    return object : FloatValueHolder() {
+        override fun getValue(): Float {
+            return getter.invoke()
+        }
+
+        override fun setValue(value: Float) {
+            setter.invoke(value)
+        }
+    }
 }

stitchlv/compose-multiplatform-core

dynamic-animation/ktx/src/androidTest/java/androidx/dynamicanimation/tests/DynamicAnimationTest.kt
@@ -1,0 +1,138 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.dynamicanimation.tests
+
+import android.view.View
+
+import androidx.dynamicanimation.animation.DynamicAnimation
+import androidx.dynamicanimation.animation.SpringForce
+import androidx.dynamicanimation.animation.flingAnimationOf
+import androidx.dynamicanimation.animation.springAnimationOf
+import androidx.dynamicanimation.animation.withSpringForceProperties
+import androidx.dynamicanimation.ktx.test.R
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.rule.ActivityTestRule
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertNull
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.timeout
+import org.mockito.Mockito.verify
+
+@SmallTest
+@RunWith(AndroidJUnit4::class)
+class DynamicAnimationTest {
+
+    @get:Rule val activityTestRule = ActivityTestRule<AnimationActivity>(
+        AnimationActivity::class.java)
+
+    private lateinit var view: View
+
+    @Before fun setup() {
+        view = activityTestRule.getActivity().findViewById(R.id.anim_view)
+    }
+
+    /**
+     * Test extension for creating [FlingAnimation]
+     */
+    @Test fun testCreateFlingAnimation() {
+        val flingAnimation = flingAnimationOf(view::setAlpha, view::getAlpha)
+        assertNotNull(flingAnimation)
+        assertTrue(flingAnimation.friction == 1f)
+        flingAnimation.friction = 1.5f
+        assertTrue(flingAnimation.friction == 1.5f)
+    }
+
+    /**
+     * Test extension for creating [SpringAnimation]
+     */
+    @Test fun testCreateSpringAnimation() {
+        val springAnimationWithoutFinalPosition =
+            springAnimationOf(view::setScaleX, view::getScaleX)
+        assertNotNull(springAnimationWithoutFinalPosition)
+        assertNull(springAnimationWithoutFinalPosition.spring)
+        val springAnimationWithFinalPosition =
+            springAnimationOf(view::setScaleX, view::getScaleX, 100f)
+        assertNotNull(springAnimationWithFinalPosition)
+        assertNotNull(springAnimationWithFinalPosition.spring)
+        assertEquals(springAnimationWithFinalPosition.spring.finalPosition, 100f)
+
+        val listener: DynamicAnimation.OnAnimationEndListener = mock(
+                DynamicAnimation.OnAnimationEndListener::class.java)
+        springAnimationWithFinalPosition.addEndListener(listener)
+        InstrumentationRegistry.getInstrumentation().runOnMainSync({
+                springAnimationWithFinalPosition.setStartValue(0f).start()
+        })
+        assertTrue(springAnimationWithFinalPosition.isRunning())
+
+        InstrumentationRegistry.getInstrumentation().runOnMainSync({
+                springAnimationWithFinalPosition.skipToEnd()
+        })
+
+        verify(listener, timeout(2000)).onAnimationEnd(springAnimationWithFinalPosition, false,
+            100f, 0f)
+
+        assertEquals(100f, view.getScaleX())
+    }
+
+    /**
+     * Test extension for setting up [SpringForce]
+     */
+    @Test fun testSpringForce() {
+        val springAnimationWithoutFinalPosition = springAnimationOf(
+            view::setTranslationX, view::getTranslationX)
+            .withSpringForceProperties {
+                finalPosition = 100f
+                dampingRatio = SpringForce.DAMPING_RATIO_HIGH_BOUNCY
+                stiffness = SpringForce.STIFFNESS_HIGH
+            }
+        assertNotNull(springAnimationWithoutFinalPosition.spring)
+        assertEquals(springAnimationWithoutFinalPosition.spring.finalPosition, 100f)
+        assertEquals(
+            springAnimationWithoutFinalPosition.spring.dampingRatio,
+            SpringForce.DAMPING_RATIO_HIGH_BOUNCY
+        )
+        assertEquals(
+            springAnimationWithoutFinalPosition.spring.stiffness,
+            SpringForce.STIFFNESS_HIGH
+        )
+        val springAnimationWithFinalPosition = springAnimationOf(
+            view::setTranslationX, view::getTranslationX, 120f)
+            .withSpringForceProperties {
+                dampingRatio = SpringForce.DAMPING_RATIO_LOW_BOUNCY
+                stiffness = SpringForce.STIFFNESS_LOW
+            }
+        assertNotNull(springAnimationWithFinalPosition.spring)
+        assertEquals(springAnimationWithFinalPosition.spring.finalPosition, 120f)
+        assertEquals(
+            springAnimationWithFinalPosition.spring.dampingRatio,
+            SpringForce.DAMPING_RATIO_LOW_BOUNCY
+        )
+        assertEquals(
+            springAnimationWithFinalPosition.spring.stiffness,
+            SpringForce.STIFFNESS_LOW
+        )
+    }
+}

dynamic-animation/ktx/src/main/java/androidx/dynamicanimation/animation/DynamicAnimation.kt
@@ -17,32 +17,39 @@
 package androidx.dynamicanimation.animation
 
 /**
- * Creates [FlingAnimation] for object.
+ * Creates [FlingAnimation] for a property that can be accessed via the provided setter and getter.
+ * For example, the following sample code creates a [FlingAnimation] for the alpha property of a
+ * [View] object named `view`:
+ * `flingAnimationOf(view::setAlpha, view::getAlpha)`
  *
- * @param property object's property to be animated.
+ * @param setter The function that mutates the property being animated
+ * @param getter The function that returns the value of the property
  * @return [FlingAnimation]
  */
-inline fun <K> K.flingAnimationOf(property: FloatPropertyCompat<K>): FlingAnimation {
-    return FlingAnimation(this, property)
+fun flingAnimationOf(setter: (Float) -> Unit, getter: () -> Float): FlingAnimation {
+    return FlingAnimation(createFloatValueHolder(setter, getter))
 }
 
 /**
- * Creates [SpringAnimation] for object.
+ * Creates [SpringAnimation] for a property that can be accessed via the provided setter and getter.
  * If finalPosition is not [Float.NaN] then create [SpringAnimation] with
  * [SpringForce.mFinalPosition].
  *
- * @param property object's property to be animated.
+ * @param setter The function that mutates the property being animated
+ * @param getter The function that returns the value of the property
  * @param finalPosition [SpringForce.mFinalPosition] Final position of spring.
  * @return [SpringAnimation]
  */
-inline fun <K> K.springAnimationOf(
-    property: FloatPropertyCompat<K>,
+fun springAnimationOf(
+    setter: (Float) -> Unit,
+    getter: () -> Float,
     finalPosition: Float = Float.NaN
 ): SpringAnimation {
+    val valueHolder = createFloatValueHolder(setter, getter)
     return if (finalPosition.isNaN()) {
-        SpringAnimation(this, property)
+        SpringAnimation(valueHolder)
     } else {
-        SpringAnimation(this, property, finalPosition)
+        SpringAnimation(valueHolder, finalPosition)
     }
 }
 
@@ -64,4 +71,16 @@ inline fun SpringAnimation.withSpringForceProperties(
     }
     spring.func()
     return this
+}
+
+private fun createFloatValueHolder(setter: (Float) -> Unit, getter: () -> Float): FloatValueHolder {
+    return object : FloatValueHolder() {
+        override fun getValue(): Float {
+            return getter.invoke()
+        }
+
+        override fun setValue(value: Float) {
+            setter.invoke(value)
+        }
+    }
 }