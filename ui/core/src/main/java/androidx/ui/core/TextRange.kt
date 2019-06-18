aosp-mirror/platform_frameworks_support

Implement DeleteSurroundingText
Since DeleteSurroundingText needs composition and selection adjustment,
add new method into EditingBuffer.

Bug: 113819017
Test: ./gradlew test
Change-Id: Icabff5e64dab22a610c37315fe2fb0fffe550aaf

ui/core/src/main/java/androidx/ui/core/TextRange.kt
@@ -36,4 +36,24 @@ data class TextRange(val start: Int, val end: Int) {
      * Returns true if the range is collapsed
      */
     val collapsed: Boolean get() = start == end
+
+    /**
+     * Returns the length of the range.
+     */
+    val length: Int get() = end - start
+
+    /**
+     * Returns true if the given range has intersection with this range
+     */
+    fun intersect(other: TextRange): Boolean = start < other.end && other.start < end
+
+    /**
+     * Returns true if this range covers including equals with the given range.
+     */
+    fun contains(other: TextRange): Boolean = start <= other.start && other.end <= end
+
+    /**
+     * Returns true if the given offset is a part of this range.
+     */
+    fun contains(offset: Int): Boolean = start <= offset && offset < end
 }

ui/core/src/main/java/androidx/ui/input/EditingBuffer.kt
@@ -175,6 +175,105 @@ class EditingBuffer(
     }
 
     /**
+     * Remove the given range of text.
+     *
+     * Different from replace method, this doesn't move cursor location to the end of modified text.
+     * Instead, preserve the selection with adjusting the deleted text.
+     */
+    fun delete(start: Int, end: Int) {
+        // TODO(nona): Remove TextRange object creation if this is performance critical.
+        val delRange = TextRange(start, end)
+        if (delRange.intersect(TextRange(selectionStart, selectionEnd))) {
+            // Currently only target for deleteSurroundingText/deleteSurroundingTextInCodePoints.
+            TODO("support deletion within selection range.")
+        }
+
+        gapBuffer.replace(start, end, "")
+        if (end <= selectionStart) {
+            selectionStart -= delRange.length
+            selectionEnd -= delRange.length
+        }
+
+        if (!hasComposition()) {
+            return
+        }
+
+        val compRange = TextRange(compositionStart, compositionEnd)
+
+        // Following figure shows the deletion range and composition range.
+        // |---| represents a range to be deleted.
+        // |===| represents a composition range.
+        if (delRange.intersect(compRange)) {
+            if (delRange.contains(compRange)) {
+                // Input:
+                //   Buffer     : ABCDEFGHIJKLMNOPQRSTUVWXYZ
+                //   Delete     :      |-------------|
+                //   Composition:          |======|
+                //
+                // Result:
+                //   Buffer     : ABCDETUVWXYZ
+                //   Composition:
+                compositionStart = NOWHERE
+                compositionEnd = NOWHERE
+            } else if (compRange.contains(delRange)) {
+                // Input:
+                //   Buffer     : ABCDEFGHIJKLMNOPQRSTUVWXYZ
+                //   Delete     :          |------|
+                //   Composition:        |==========|
+                //
+                // Result:
+                //   Buffer     : ABCDEFGHIQRSTUVWXYZ
+                //   Composition:        |===|
+                compositionEnd -= delRange.length
+            } else if (delRange.contains(compositionStart)) {
+                // Input:
+                //   Buffer     : ABCDEFGHIJKLMNOPQRSTUVWXYZ
+                //   Delete     :      |---------|
+                //   Composition:            |========|
+                //
+                // Result:
+                //   Buffer     : ABCDEFPQRSTUVWXYZ
+                //   Composition:       |=====|
+                compositionStart = delRange.start
+                compositionEnd -= delRange.length
+            } else { // delRange contains compositionEnd
+                // Input:
+                //   Buffer     : ABCDEFGHIJKLMNOPQRSTUVWXYZ
+                //   Delete     :         |---------|
+                //   Composition:    |=======|
+                //
+                // Result:
+                //   Buffer     : ABCDEFGHSTUVWXYZ
+                //   Composition:    |====|
+                compositionEnd = delRange.start
+            }
+        } else {
+            if (compositionStart <= delRange.start) {
+                // Input:
+                //   Buffer     : ABCDEFGHIJKLMNOPQRSTUVWXYZ
+                //   Delete     :            |-------|
+                //   Composition:  |=======|
+                //
+                // Result:
+                //   Buffer     : ABCDEFGHIJKLTUVWXYZ
+                //   Composition:  |=======|
+                // do nothing
+            } else {
+                // Input:
+                //   Buffer     : ABCDEFGHIJKLMNOPQRSTUVWXYZ
+                //   Delete     :  |-------|
+                //   Composition:            |=======|
+                //
+                // Result:
+                //   Buffer     : AJKLMNOPQRSTUVWXYZ
+                //   Composition:    |=======|
+                compositionStart -= delRange.length
+                compositionEnd -= delRange.length
+            }
+        }
+    }
+
+    /**
      * Mark the specified area of the text as selected text.
      *
      * You can set cursor by specifying the same value to `start` and `end`.

ui/core/src/test/java/androidx/ui/core/TextRangeTest.kt
@@ -40,4 +40,32 @@ class TextRangeTest {
         assertTrue(TextRange(1, 1).collapsed)
         assertFalse(TextRange(1, 2).collapsed)
     }
+
+    @Test
+    fun intersectTest() {
+        assertTrue(TextRange(0, 2).intersect(TextRange(1, 2)))
+        assertTrue(TextRange(0, 1).intersect(TextRange(0, 1)))
+        assertTrue(TextRange(0, 2).intersect(TextRange(0, 1)))
+        assertTrue(TextRange(0, 1).intersect(TextRange(0, 2)))
+        assertFalse(TextRange(0, 1).intersect(TextRange(1, 2)))
+        assertFalse(TextRange(0, 1).intersect(TextRange(2, 3)))
+        assertFalse(TextRange(1, 2).intersect(TextRange(0, 1)))
+        assertFalse(TextRange(2, 3).intersect(TextRange(0, 1)))
+    }
+
+    @Test
+    fun containsRangeTest() {
+        assertTrue(TextRange(0, 2).contains(TextRange(0, 1)))
+        assertTrue(TextRange(0, 2).contains(TextRange(0, 2)))
+        assertFalse(TextRange(0, 2).contains(TextRange(0, 3)))
+        assertFalse(TextRange(0, 2).contains(TextRange(1, 3)))
+    }
+
+    @Test
+    fun containsOffsetTest() {
+        assertTrue(TextRange(0, 2).contains(0))
+        assertTrue(TextRange(0, 2).contains(1))
+        assertFalse(TextRange(0, 2).contains(2))
+        assertFalse(TextRange(0, 2).contains(3))
+    }
 }

stitchlv/compose-multiplatform-core

ui/core/src/main/java/androidx/ui/core/TextRange.kt
@@ -36,4 +36,24 @@ data class TextRange(val start: Int, val end: Int) {
      * Returns true if the range is collapsed
      */
     val collapsed: Boolean get() = start == end
+
+    /**
+     * Returns the length of the range.
+     */
+    val length: Int get() = end - start
+
+    /**
+     * Returns true if the given range has intersection with this range
+     */
+    fun intersects(other: TextRange): Boolean = start < other.end && other.start < end
+
+    /**
+     * Returns true if this range covers including equals with the given range.
+     */
+    fun contains(other: TextRange): Boolean = start <= other.start && other.end <= end
+
+    /**
+     * Returns true if the given offset is a part of this range.
+     */
+    fun contains(offset: Int): Boolean = start <= offset && offset < end
 }

ui/core/src/main/java/androidx/ui/input/EditingBuffer.kt
@@ -175,6 +175,105 @@ class EditingBuffer(
     }
 
     /**
+     * Remove the given range of text.
+     *
+     * Different from replace method, this doesn't move cursor location to the end of modified text.
+     * Instead, preserve the selection with adjusting the deleted text.
+     */
+    fun delete(start: Int, end: Int) {
+        // TODO(nona): Remove TextRange object creation if this is performance critical.
+        val deleteRange = TextRange(start, end)
+        if (deleteRange.intersects(TextRange(selectionStart, selectionEnd))) {
+            // Currently only target for deleteSurroundingText/deleteSurroundingTextInCodePoints.
+            TODO("support deletion within selection range.")
+        }
+
+        gapBuffer.replace(start, end, "")
+        if (end <= selectionStart) {
+            selectionStart -= deleteRange.length
+            selectionEnd -= deleteRange.length
+        }
+
+        if (!hasComposition()) {
+            return
+        }
+
+        val compositionRange = TextRange(compositionStart, compositionEnd)
+
+        // Following figure shows the deletion range and composition range.
+        // |---| represents a range to be deleted.
+        // |===| represents a composition range.
+        if (deleteRange.intersects(compositionRange)) {
+            if (deleteRange.contains(compositionRange)) {
+                // Input:
+                //   Buffer     : ABCDEFGHIJKLMNOPQRSTUVWXYZ
+                //   Delete     :      |-------------|
+                //   Composition:          |======|
+                //
+                // Result:
+                //   Buffer     : ABCDETUVWXYZ
+                //   Composition:
+                compositionStart = NOWHERE
+                compositionEnd = NOWHERE
+            } else if (compositionRange.contains(deleteRange)) {
+                // Input:
+                //   Buffer     : ABCDEFGHIJKLMNOPQRSTUVWXYZ
+                //   Delete     :          |------|
+                //   Composition:        |==========|
+                //
+                // Result:
+                //   Buffer     : ABCDEFGHIQRSTUVWXYZ
+                //   Composition:        |===|
+                compositionEnd -= deleteRange.length
+            } else if (deleteRange.contains(compositionStart)) {
+                // Input:
+                //   Buffer     : ABCDEFGHIJKLMNOPQRSTUVWXYZ
+                //   Delete     :      |---------|
+                //   Composition:            |========|
+                //
+                // Result:
+                //   Buffer     : ABCDEFPQRSTUVWXYZ
+                //   Composition:       |=====|
+                compositionStart = deleteRange.start
+                compositionEnd -= deleteRange.length
+            } else { // deleteRange contains compositionEnd
+                // Input:
+                //   Buffer     : ABCDEFGHIJKLMNOPQRSTUVWXYZ
+                //   Delete     :         |---------|
+                //   Composition:    |=======|
+                //
+                // Result:
+                //   Buffer     : ABCDEFGHSTUVWXYZ
+                //   Composition:    |====|
+                compositionEnd = deleteRange.start
+            }
+        } else {
+            if (compositionStart <= deleteRange.start) {
+                // Input:
+                //   Buffer     : ABCDEFGHIJKLMNOPQRSTUVWXYZ
+                //   Delete     :            |-------|
+                //   Composition:  |=======|
+                //
+                // Result:
+                //   Buffer     : ABCDEFGHIJKLTUVWXYZ
+                //   Composition:  |=======|
+                // do nothing
+            } else {
+                // Input:
+                //   Buffer     : ABCDEFGHIJKLMNOPQRSTUVWXYZ
+                //   Delete     :  |-------|
+                //   Composition:            |=======|
+                //
+                // Result:
+                //   Buffer     : AJKLMNOPQRSTUVWXYZ
+                //   Composition:    |=======|
+                compositionStart -= deleteRange.length
+                compositionEnd -= deleteRange.length
+            }
+        }
+    }
+
+    /**
      * Mark the specified area of the text as selected text.
      *
      * You can set cursor by specifying the same value to `start` and `end`.

ui/core/src/test/java/androidx/ui/core/TextRangeTest.kt
@@ -34,10 +34,38 @@ class TextRangeTest {
     }
 
     @Test
-    fun rangeCollupsedTest() {
+    fun test_range_collapsed() {
         assertTrue(TextRange(0, 0).collapsed)
         assertFalse(TextRange(0, 1).collapsed)
         assertTrue(TextRange(1, 1).collapsed)
         assertFalse(TextRange(1, 2).collapsed)
+    }
+
+    @Test
+    fun test_intersects() {
+        assertTrue(TextRange(0, 2).intersects(TextRange(1, 2)))
+        assertTrue(TextRange(0, 1).intersects(TextRange(0, 1)))
+        assertTrue(TextRange(0, 2).intersects(TextRange(0, 1)))
+        assertTrue(TextRange(0, 1).intersects(TextRange(0, 2)))
+        assertFalse(TextRange(0, 1).intersects(TextRange(1, 2)))
+        assertFalse(TextRange(0, 1).intersects(TextRange(2, 3)))
+        assertFalse(TextRange(1, 2).intersects(TextRange(0, 1)))
+        assertFalse(TextRange(2, 3).intersects(TextRange(0, 1)))
+    }
+
+    @Test
+    fun test_contains_range() {
+        assertTrue(TextRange(0, 2).contains(TextRange(0, 1)))
+        assertTrue(TextRange(0, 2).contains(TextRange(0, 2)))
+        assertFalse(TextRange(0, 2).contains(TextRange(0, 3)))
+        assertFalse(TextRange(0, 2).contains(TextRange(1, 3)))
+    }
+
+    @Test
+    fun test_contains_offset() {
+        assertTrue(TextRange(0, 2).contains(0))
+        assertTrue(TextRange(0, 2).contains(1))
+        assertFalse(TextRange(0, 2).contains(2))
+        assertFalse(TextRange(0, 2).contains(3))
     }
 }