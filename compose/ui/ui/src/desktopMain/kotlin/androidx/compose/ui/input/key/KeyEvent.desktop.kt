stitchlv/compose-multiplatform-core

commit 7c68ebcd57c287686e19511ae3a35adb890b1cbb

Finalize skiko events refactor (#1245)
## Proposed Changes

- Revert public event structures for web and native: `NativeKeyEvent`
and duplicated internal: `NativePointerEvent`, `PointerEventRecord`,
`MouseButtons`
- Move a few duplicated actuals to skiko sourceset
- `actual typealias NativeKeyEvent = Any` for all skiko platforms
- New public `PointerEvent.appkitEventOrNull` getter for macOS-native
- New public `KeyEvent.uikitEventOrNull` getter for iOS
- Web: use platform's buttons tracking and subscribe to enter/exit
events
- macOS-native: restore mouse wheel events
- iOS: `UIPress.phase` for the event type instead of passing manually
- Desktop: Store `InternalKeyEvent` in `nativeKeyEvent` instead of raw
AWT event to match other platforms
 
## Testing

Test: CI + mpp input

compose/ui/ui/api/desktop/ui.api
@@ -1654,7 +1654,9 @@ public final class androidx/compose/ui/input/key/KeyEventType$Companion {
 	public final fun getUnknown-CS__XNY ()I
 }
 
-public final class androidx/compose/ui/input/key/KeyEvent_desktopKt {
+public final class androidx/compose/ui/input/key/KeyEvent_skikoKt {
+	public static final fun KeyEvent-4Zc56iI (JIIZZZZLjava/lang/Object;)Ljava/lang/Object;
+	public static synthetic fun KeyEvent-4Zc56iI$default (JIIZZZZLjava/lang/Object;ILjava/lang/Object;)Ljava/lang/Object;
 	public static final fun getKey-ZmokQxo (Ljava/lang/Object;)J
 	public static final fun getType-ZmokQxo (Ljava/lang/Object;)I
 	public static final fun getUtf16CodePoint-ZmokQxo (Ljava/lang/Object;)I

compose/ui/ui/src/desktopMain/kotlin/androidx/compose/ui/input/key/KeyEvent.desktop.kt
@@ -16,79 +16,52 @@
 
 package androidx.compose.ui.input.key
 
-import androidx.compose.ui.awt.awtEventOrNull
+import androidx.compose.ui.input.pointer.PointerKeyboardModifiers
+import java.awt.Toolkit
 import java.awt.event.KeyEvent.KEY_LOCATION_STANDARD
 import java.awt.event.KeyEvent.KEY_LOCATION_UNKNOWN
 import java.awt.event.KeyEvent.KEY_PRESSED
 import java.awt.event.KeyEvent.KEY_RELEASED
-import java.awt.event.KeyEvent.VK_UNDEFINED
-
-/**
- * The native desktop [KeyEvent][KeyEventAwt].
- */
-actual typealias NativeKeyEvent = Any
-
-/**
- * The key that was pressed.
- */
-actual val KeyEvent.key: Key
-    get() = Key(
-        awtEventOrNull?.keyCode ?: VK_UNDEFINED,
-        awtEventOrNull?.keyLocationForCompose ?: KEY_LOCATION_STANDARD
-    )
 
 private val java.awt.event.KeyEvent.keyLocationForCompose get() =
     if (keyLocation == KEY_LOCATION_UNKNOWN) KEY_LOCATION_STANDARD else keyLocation
 
-/**
- * The UTF16 value corresponding to the key event that was pressed. The unicode character
- * takes into account any meta keys that are pressed (eg. Pressing shift results in capital
- * alphabets). The UTF16 value uses the
- * [U+n notation][http://www.unicode.org/reports/tr27/#notation] of the Unicode Standard.
- *
- * An [Int] is used instead of a [Char] so that we can support supplementary characters. The
- * Unicode Standard allows for characters whose representation requires more than 16 bits.
- * The range of legal code points is U+0000 to U+10FFFF, known as Unicode scalar value.
- *
- * The set of characters from U+0000 to U+FFFF is sometimes referred to as the Basic
- * Multilingual Plane (BMP). Characters whose code points are greater than U+FFFF are called
- * supplementary characters. In this representation, supplementary characters are represented
- * as a pair of char values, the first from the high-surrogates range, (\uD800-\uDBFF), the
- * second from the low-surrogates range (\uDC00-\uDFFF).
- */
-actual val KeyEvent.utf16CodePoint: Int
-    get() = awtEventOrNull?.keyChar?.code ?: 0
+internal fun java.awt.event.KeyEvent.toComposeEvent() = KeyEvent(
+    nativeKeyEvent = InternalKeyEvent(
+        key = Key(
+            nativeKeyCode = keyCode,
+            nativeKeyLocation = keyLocationForCompose
+        ),
+        type = when (id) {
+            KEY_PRESSED -> KeyEventType.KeyDown
+            KEY_RELEASED -> KeyEventType.KeyUp
+            else -> KeyEventType.Unknown
+        },
+        codePoint = keyChar.code,
+        modifiers = toPointerKeyboardModifiers(),
+        nativeEvent = this
+    )
+)
 
-/**
- * The [type][KeyEventType] of key event.
- */
-actual val KeyEvent.type: KeyEventType
-    get() = when (awtEventOrNull?.id) {
-        KEY_PRESSED -> KeyEventType.KeyDown
-        KEY_RELEASED -> KeyEventType.KeyUp
-        else -> KeyEventType.Unknown
-    }
+private fun getLockingKeyStateSafe(
+    mask: Int
+): Boolean = try {
+    Toolkit.getDefaultToolkit().getLockingKeyState(mask)
+} catch (_: Exception) {
+    false
+}
 
-/**
- * Indicates whether the Alt key is pressed.
- */
-actual val KeyEvent.isAltPressed: Boolean
-    get() = awtEventOrNull?.isAltDown == true || awtEventOrNull?.isAltGraphDown == true
-
-/**
- * Indicates whether the Ctrl key is pressed.
- */
-actual val KeyEvent.isCtrlPressed: Boolean
-    get() = awtEventOrNull?.isControlDown == true
-
-/**
- * Indicates whether the Meta key is pressed.
- */
-actual val KeyEvent.isMetaPressed: Boolean
-    get() = awtEventOrNull?.isMetaDown == true
-
-/**
- * Indicates whether the Shift key is pressed.
- */
-actual val KeyEvent.isShiftPressed: Boolean
-    get() = awtEventOrNull?.isShiftDown == true
+private fun java.awt.event.KeyEvent.toPointerKeyboardModifiers(): PointerKeyboardModifiers {
+    return PointerKeyboardModifiers(
+        isCtrlPressed = isControlDown,
+        isMetaPressed = isMetaDown,
+        isAltPressed = isAltDown,
+        isShiftPressed = isShiftDown,
+        isAltGraphPressed = isAltGraphDown,
+        isSymPressed = false, // no sym in awtEvent?
+        isFunctionPressed = false, // no Fn in awtEvent?
+        isCapsLockOn = getLockingKeyStateSafe(java.awt.event.KeyEvent.VK_CAPS_LOCK),
+        isScrollLockOn = getLockingKeyStateSafe(java.awt.event.KeyEvent.VK_SCROLL_LOCK),
+        isNumLockOn = getLockingKeyStateSafe(java.awt.event.KeyEvent.VK_NUM_LOCK),
+    )
+}

compose/ui/ui/src/skikoMain/kotlin/androidx/compose/ui/input/key/KeyEvent.skiko.kt
@@ -1,0 +1,101 @@
+/*
+ * Copyright 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.compose.ui.input.key
+
+import androidx.compose.ui.InternalComposeUiApi
+import androidx.compose.ui.input.pointer.PointerKeyboardModifiers
+import androidx.compose.ui.input.pointer.isAltGraphPressed
+import androidx.compose.ui.input.pointer.isAltPressed
+import androidx.compose.ui.input.pointer.isCtrlPressed
+import androidx.compose.ui.input.pointer.isMetaPressed
+import androidx.compose.ui.input.pointer.isShiftPressed
+
+actual typealias NativeKeyEvent = Any
+
+internal data class InternalKeyEvent(
+    val key: Key,
+    val type: KeyEventType,
+    val codePoint: Int,
+    val modifiers: PointerKeyboardModifiers, // Reuse pointer modifiers
+
+    val nativeEvent: Any? = null
+)
+
+internal val KeyEvent.internal: InternalKeyEvent
+    get() = nativeKeyEvent as InternalKeyEvent
+
+actual val KeyEvent.key: Key
+    get() = internal.key
+
+actual val KeyEvent.utf16CodePoint: Int
+    get() = internal.codePoint
+
+actual val KeyEvent.type: KeyEventType
+    get() = internal.type
+
+actual val KeyEvent.isAltPressed: Boolean
+    get() = internal.modifiers.isAltPressed || internal.modifiers.isAltGraphPressed
+
+actual val KeyEvent.isCtrlPressed: Boolean
+    get() = internal.modifiers.isCtrlPressed
+
+actual val KeyEvent.isMetaPressed: Boolean
+    get() = internal.modifiers.isMetaPressed
+
+actual val KeyEvent.isShiftPressed: Boolean
+    get() = internal.modifiers.isShiftPressed
+
+@InternalComposeUiApi
+fun KeyEvent(
+    key: Key,
+    type: KeyEventType,
+    codePoint: Int = 0,
+    isCtrlPressed: Boolean = false,
+    isMetaPressed: Boolean = false,
+    isAltPressed: Boolean = false,
+    isShiftPressed: Boolean = false,
+    nativeEvent: Any? = null
+) = KeyEvent(
+    nativeKeyEvent = InternalKeyEvent(
+        key = key,
+        type = type,
+        codePoint = codePoint,
+        modifiers = PointerKeyboardModifiers(
+            isCtrlPressed = isCtrlPressed,
+            isMetaPressed = isMetaPressed,
+            isAltPressed = isAltPressed,
+            isShiftPressed = isShiftPressed
+        ),
+        nativeEvent = nativeEvent as? InternalKeyEvent
+    )
+)
+
+internal fun KeyEvent.copy(
+    key: Key = this.internal.key,
+    type: KeyEventType = this.internal.type,
+    codePoint: Int = this.internal.codePoint,
+    modifiers: PointerKeyboardModifiers = this.internal.modifiers,
+    nativeEvent: Any? = this.internal.nativeEvent
+) = KeyEvent(
+    nativeKeyEvent = InternalKeyEvent(
+        key = key,
+        type = type,
+        codePoint = codePoint,
+        modifiers = modifiers,
+        nativeEvent = nativeEvent
+    )
+)

compose/foundation/foundation/src/desktopMain/kotlin/androidx/compose/foundation/Clickable.desktop.kt
@@ -58,28 +58,8 @@ import androidx.compose.ui.util.fastAll
 import androidx.compose.ui.util.fastAny
 import java.awt.event.KeyEvent.VK_ENTER
 
-// TODO(https://github.com/JetBrains/compose-multiplatform/issues/3341): support isComposeRootInScrollableContainer
-internal actual fun CompositionLocalConsumerModifierNode
-    .isComposeRootInScrollableContainer(): Boolean {
-    return false
-}
-
 // TODO: b/168524931 - should this depend on the input device?
 internal actual val TapIndicationDelay: Long = 0L
-
-/**
- * Whether the specified [KeyEvent] should trigger a press for a clickable component, i.e. whether
- * it is associated with a press of the enter key.
- */
-internal actual val KeyEvent.isPress: Boolean
-    get() = type == KeyDown && key.nativeKeyCode == VK_ENTER
-
-/**
- * Whether the specified [KeyEvent] should trigger a click for a clickable component, i.e. whether
- * it is associated with a release of the enter key.
- */
-internal actual val KeyEvent.isClick: Boolean
-    get() = type == KeyUp && key.nativeKeyCode == VK_ENTER
 
 @Immutable @ExperimentalFoundationApi
 @Deprecated(

compose/foundation/foundation/src/desktopTest/kotlin/androidx/compose/foundation/internal/KeyEventUtils.desktopMain.kt
@@ -1,52 +1,0 @@
-/*
- * Copyright 2022 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package androidx.compose.foundation.internal
-
-import androidx.compose.ui.input.key.Key
-import androidx.compose.ui.input.key.KeyEventType
-import androidx.compose.ui.input.key.nativeKeyCode
-import androidx.compose.ui.input.key.nativeKeyLocation
-import java.awt.Component
-import java.awt.event.KeyEvent
-
-private object DummyComponent : Component()
-/**
- * The [KeyEvent] is usually created by the system. This function creates an instance of
- * [KeyEvent] that can be used in tests.
- */
-internal actual fun keyEvent(
-    key: Key,
-    keyEventType: KeyEventType,
-    modifiers: Int
-): androidx.compose.ui.input.key.KeyEvent {
-    val action = when (keyEventType) {
-        KeyEventType.KeyDown -> java.awt.event.KeyEvent.KEY_PRESSED
-        KeyEventType.KeyUp -> java.awt.event.KeyEvent.KEY_RELEASED
-        else -> error("Unknown key event type")
-    }
-    return androidx.compose.ui.input.key.KeyEvent(
-        KeyEvent(
-            DummyComponent,
-            action,
-            0L,
-            modifiers,
-            key.nativeKeyCode,
-            KeyEvent.getKeyText(key.nativeKeyCode)[0],
-            key.nativeKeyLocation
-        )
-    )
-}

compose/foundation/foundation/src/jsNativeTest/kotlin/androidx/compose/foundation/internal/KeyEventUtils.jsNativeMain.kt
@@ -1,39 +1,0 @@
-/*
- * Copyright 2022 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package androidx.compose.foundation.internal
-
-import androidx.compose.ui.input.key.NativeKeyEvent
-import androidx.compose.ui.input.key.Key
-import androidx.compose.ui.input.key.KeyEvent
-import androidx.compose.ui.input.key.KeyEventType
-
-
-internal actual fun keyEvent(
-    key: Key,
-    keyEventType: KeyEventType,
-    modifiers: Int
-): KeyEvent {
-    return KeyEvent(
-        NativeKeyEvent(
-            key = key,
-            value = null,
-            modifiers = modifiers,
-            kind = keyEventType,
-            timestamp = 0L
-        )
-    )
-}

compose/foundation/foundation/src/macosMain/kotlin/androidx/compose/foundation/gestures/MacosScrollable.macos.kt
@@ -14,17 +14,15 @@
  * limitations under the License.
  */
 
-@file:Suppress("DEPRECATION")
-
 package androidx.compose.foundation.gestures
 
+import androidx.compose.ui.appkit.appkitEventOrNull
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.input.pointer.PointerEvent
 import androidx.compose.ui.node.CompositionLocalConsumerModifierNode
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
-import platform.AppKit.NSEvent
 
 internal actual fun CompositionLocalConsumerModifierNode.platformScrollConfig(): ScrollConfig =
     MacOsScrollConfig
@@ -32,8 +30,7 @@ internal actual fun CompositionLocalConsumerModifierNode.platformScrollConfig():
 private object MacOsScrollConfig : ScrollConfig {
     // See https://developer.apple.com/documentation/appkit/nsevent/1535387-scrollingdeltay
     override fun Density.calculateMouseWheelScroll(event: PointerEvent, bounds: IntSize): Offset {
-        // TODO: Replace with event.nsEventOrNull
-        val e: NSEvent = event.nativeEvent as? NSEvent? ?: return Offset.Zero
+        val e = event.appkitEventOrNull ?: return Offset.Zero
 
         // The multiplier value was derived from desktop MacOSCocoaConfig
         val multiplier = if (e.hasPreciseScrollingDeltas) 1.0F else 10.dp.toPx()

compose/foundation/foundation/src/macosMain/kotlin/androidx/compose/foundation/text/TextFieldKeyInput.macos.kt
@@ -18,7 +18,9 @@ package androidx.compose.foundation.text
 
 import androidx.compose.ui.input.key.KeyEvent
 import androidx.compose.ui.input.key.KeyEventType
+import androidx.compose.ui.input.key.isMetaPressed
+import androidx.compose.ui.input.key.type
+import androidx.compose.ui.input.key.utf16CodePoint
 
 actual val KeyEvent.isTypedEvent: Boolean
-    get() = nativeKeyEvent.kind == KeyEventType.KeyDown
-        && nativeKeyEvent.value?.isNullOrEmpty() == true
+    get() = type == KeyEventType.KeyDown && !isMetaPressed && utf16CodePoint != 0

commit 3edaf80b89ffda98640e59fa5332eee72cfd55ee

Fix `KeyEvent` binary compatibility (#1310)
Fix binary compatibility with pre-1.6.10. Broken in #1245

compose/ui/ui/api/desktop/ui.api
@@ -1654,7 +1654,7 @@ public final class androidx/compose/ui/input/key/KeyEventType$Companion {
 	public final fun getUnknown-CS__XNY ()I
 }
 
-public final class androidx/compose/ui/input/key/KeyEvent_skikoKt {
+public final class androidx/compose/ui/input/key/KeyEvent_desktopKt {
 	public static final fun KeyEvent-4Zc56iI (JIIZZZZLjava/lang/Object;)Ljava/lang/Object;
 	public static synthetic fun KeyEvent-4Zc56iI$default (JIIZZZZLjava/lang/Object;ILjava/lang/Object;)Ljava/lang/Object;
 	public static final fun getKey-ZmokQxo (Ljava/lang/Object;)J

compose/ui/ui/src/desktopMain/kotlin/androidx/compose/ui/input/key/KeyEvent.desktop.kt
@@ -14,6 +14,9 @@
  * limitations under the License.
  */
 
+@file:JvmName("KeyEvent_desktopKt")
+@file:JvmMultifileClass
+
 package androidx.compose.ui.input.key
 
 import androidx.compose.ui.input.pointer.PointerKeyboardModifiers

compose/ui/ui/src/skikoMain/kotlin/androidx/compose/ui/input/key/KeyEvent.skiko.kt
@@ -14,6 +14,9 @@
  * limitations under the License.
  */
 
+@file:JvmName("KeyEvent_desktopKt")
+@file:JvmMultifileClass
+
 package androidx.compose.ui.input.key
 
 import androidx.compose.ui.InternalComposeUiApi
@@ -23,6 +26,8 @@ import androidx.compose.ui.input.pointer.isAltPressed
 import androidx.compose.ui.input.pointer.isCtrlPressed
 import androidx.compose.ui.input.pointer.isMetaPressed
 import androidx.compose.ui.input.pointer.isShiftPressed
+import kotlin.jvm.JvmMultifileClass
+import kotlin.jvm.JvmName
 
 actual typealias NativeKeyEvent = Any
 